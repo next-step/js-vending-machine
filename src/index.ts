91c03f491b907309f8574a0455ab27bed159ea9b
Jay <cho2304@naver.com>
2021-12-11 22:16:16 +0900

    chore: typescript 환경설정 세팅을 한다.

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+function sayHi() {
+  console.log('hi')
+}
+
+sayHi()
---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const path = require('path')
+
+module.exports = {
+  devServer: {
+    open: true,
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    compress: true,
+    port: 3000,
+  },
+  name: 'vending-machine',
+  mode: 'development',
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+          },
+        ],
+      },
+      {
+        test: /\.html$/,
+        use: [{ loader: 'html-loader', options: { minimize: true } }],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.wasm', '.ts', '.mjs', '.cjs', '.js', '.json'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+      chunkFilename: '[id].css',
+    }),
+  ],
+  output: {
+    filename: 'index.js',
+    path: path.join(__dirname, './build'),
+  }, // 결과 파일
+}
