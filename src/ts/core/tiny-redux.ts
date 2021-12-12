c2548f8e7eda09eabcc80b7a11f0db6d1528da6d
lsw1164 <qkqkfo@gmail.com>
2021-12-13 00:59:29 +0900

    :sparkles: Add tiny-redux

---
+++ src/ts/core/tiny-redux.ts
@@ -0,0 +1,69 @@
+export function createStore<T = {}>(
+  reducer: IReducer<T>,
+  middlewareList: IMiddleware<T>[] = []
+): IStore<T> {
+  let state: T;
+  const listeners: IListener[] = [];
+
+  const publish = () => {
+    listeners.forEach(({ subscriber, context }) => {
+      subscriber.call(context);
+    });
+  };
+
+  const dispatch = (action: IAction) => {
+    state = reducer(state, action);
+    publish();
+  };
+
+  const subscribe = (subscriber: Function, context?: any) => {
+    listeners.push({ subscriber, context });
+  };
+
+  const getState = (): T => ({ ...state });
+
+  const store: IStore<T> = {
+    getState,
+    subscribe,
+    dispatch,
+  };
+
+  let lastDispatch = dispatch;
+  middlewareList.reverse().forEach((middleware) => {
+    lastDispatch = middleware(store)(lastDispatch);
+  });
+
+  return {
+    ...store,
+    dispatch: lastDispatch,
+  };
+}
+
+export const actionCreator = (type: string, payload = {}): IAction => ({
+  type,
+  payload: { ...payload },
+});
+
+export interface IStore<T> {
+  getState(): T;
+  subscribe(subscriber: Function, context?: any): void;
+  dispatch(action: IAction): void;
+}
+
+export interface IMiddleware<T = {}> {
+  (store: IStore<T>): (next: Function) => (action: IAction) => void;
+}
+
+export interface IAction {
+  type: string;
+  payload?: any;
+}
+
+export interface IReducer<T> {
+  (state: T, action: IAction): T;
+}
+
+interface IListener {
+  subscriber: Function;
+  context: any;
+}
