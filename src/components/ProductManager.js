f8c2f38a0ba1b77018fb24dc2a250eebb8e63640
Heeji <limhizy15@ajou.ac.kr>
2021-12-22 23:19:21 +0900

    [step1] feat: 탭에 맞는 UI 출력

---
+++ src/components/ProductManager.js
@@ -0,0 +1,36 @@
+import Component from "../core/Component.js";
+
+export default class App extends Component {
+  setup() {}
+
+  template() {
+    return /*html*/ `
+    <h3>상품 추가하기</h3>
+    <div class="product-container">
+        <input type="text" id="product-name-input" placeholder="상품명" />
+        <input type="number" id="product-price-input" placeholder="가격" />
+        <input type="number" id="product-quantity-input" placeholder="수량" />
+        <button id="product-add-button">추가하기</button>
+    </div>
+    <table class="product-inventory">
+        <colgroup>
+            <col style="width: 140px" />
+            <col style="width: 100px" />
+            <col style="width: 100px" />
+        </colgroup>
+        <thead>
+            <tr>
+                <th>상품명</th>
+                <th>가격</th>
+                <th>수량</th>
+            </tr>
+        </thead>
+        <tbody id="product-inventory-container"></tbody>
+    </table>
+    `;
+  }
+
+  mounted() {
+    console.log("상품추가하기");
+  }
+}
