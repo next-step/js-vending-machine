a77cba4281a9ac3c7854f8729ca1c85b968da7ff
HEEJI IM <limhizy15@ajou.ac.kr>
2021-12-23 15:41:22 +0900

    Revert "Step1 (#9)"
    This reverts commit ee137e62d59284424c6f7495f64dac6cd9071fd1.

--- src/utils/localStorage.js
+++
@@ -1,7 +0,0 @@
-export const setLocalStorage = (key, values) => {
-  return localStorage.setItem(key, JSON.stringify(values));
-};
-
-export const getLocalStorage = (key) => {
-  return JSON.parse(localStorage.getItem(key));
-};
