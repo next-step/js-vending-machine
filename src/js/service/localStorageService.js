4cc620d30e47cbc178b85f063a8642bf50972e24
g1moon <gmoon.cho@gmail.com>
2021-12-24 16:57:50 +0900

    feat: localStorage 관련 service 함수 추가

---
+++ src/js/service/localStorageService.js
@@ -0,0 +1,18 @@
+export const doesNotExistValue = (value) => {
+  const isUndefined = value === undefined;
+  const isNull = value === null;
+  const isBlank = value === '';
+
+  return isUndefined && isNull && isBlank;
+};
+
+export const getLocalStorageValueByKey = (key) => {
+  const value = window.localStorage.getItem(key);
+  if (doesNotExistValue(value)) return null;
+
+  return JSON.parse(value);
+};
+
+export const setLocalStorageValue = (key, newValue) => {
+  window.localStorage.setItem(key, JSON.stringify(newValue));
+};
