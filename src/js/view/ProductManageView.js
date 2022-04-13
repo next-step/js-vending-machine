af68d3f55de4f5b2fc9c73b31aa6760cc2b649c9
circlegiven <circlegiven@gmail.com>
2022-04-13 15:41:42 +0900

    [상품관리] 상품 추가에 대한 UI 로직 구현

--- src/js/view/ProductManageView.js
+++ src/js/view/ProductManageView.js
@@ -1,14 +1,16 @@
 import AbstractView from './AbstractView.js';
+import Product, { STEP_PRICE, MIN_PRICE, MIN_QUANTITY } from '../Product.js';
+import ProductManage from '../ProductManage.js';
 
 function productManageTemplate() {
   const $template = document.createElement('template');
   $template.innerHTML = `
     <h3>상품 추가하기</h3>
     <div class="product-container">
-      <form>
-        <input required type="text" id="product-name-input" placeholder="상품명" />
-        <input required type="number" id="product-price-input" placeholder="가격" />
-        <input required type="number" id="product-quantity-input" placeholder="수량" />
+      <form id="product-form">
+        <input required type="text" name="name" id="product-name-input" placeholder="상품명" />
+        <input required type="number" name="price" min="${MIN_PRICE}" step="${STEP_PRICE}" id="product-price-input" placeholder="가격" />
+        <input required type="number" name="quantity" min="${MIN_QUANTITY}" id="product-quantity-input" placeholder="수량" />
         <button type="submit" id="product-add-button">추가하기</button>
       </form>
     </div>
@@ -31,13 +33,48 @@ function productManageTemplate() {
   return $template.content;
 }
 
-function $productAddSubmit() {
-  return document.querySelector('#product-add-button');
+function productTemplate({ name, price, quantity }) {
+  const $template = document.createElement('template');
+  $template.innerHTML = `
+    <tr data-name="${name}" data-price="${price}" data-quantity="${quantity}">
+      <td>${name}</td>
+      <td>${price}</td>
+      <td>${quantity}</td>
+    </tr>
+  `;
+  return $template.content;
+}
+
+function $productForm() {
+  return document.querySelector('#product-form');
+}
+
+function $productInventory() {
+  return document.querySelector('#product-inventory-container');
+}
+
+function convertFormDataToObject(formData) {
+  return Object.fromEntries(formData);
 }
 
 export default class ProductManageView extends AbstractView {
+  static #updateProductList() {
+    $productInventory().replaceChildren(
+      ...ProductManage.list.map((product) => productTemplate(product))
+    );
+  }
+
   static #handleProductAdd(event) {
     event.preventDefault();
+    try {
+      const product = new Product(
+        convertFormDataToObject(new FormData(event.target))
+      );
+      ProductManage.addProduct(product);
+      ProductManageView.#updateProductList();
+    } catch (e) {
+      alert(e.message);
+    }
   }
 
   static contents() {
@@ -45,11 +82,9 @@ export default class ProductManageView extends AbstractView {
   }
 
   static eventBindings() {
-    $productAddSubmit().addEventListener(
-      'click',
+    $productForm().addEventListener(
+      'submit',
       ProductManageView.#handleProductAdd
     );
   }
-
-  static initialize() {}
 }
