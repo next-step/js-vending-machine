9e3ec0560b916d274099f1d87114587f60a87d2f
dahye1013 <alrean@naver.com>
2022-04-13 16:49:09 +0900

    test: 상품 등록 테스트 코드 추가

--- cypress/integration/veding-machine.spec.js
+++ cypress/integration/veding-machine.spec.js
@@ -7,5 +7,27 @@ describe('자판기 미션 테스트', () => {
     it('1. 초기화면 - 상품관리, 잔돈 충전, 상품 구매 버튼과 상품 추가 탭이 노출된다.', () => {
       cy.initialView();
     });
+
+    it('2. 상풍명, 가격, 수량을 입력하고 추가 버튼을 클릭하면, 상품 현황에 상품이 추가된다.', () => {
+      const item = {
+        title: '아이스 아메리카노',
+        price: 1500,
+        quantity: 5
+      };
+
+      cy.get('#product-name-input').type(item.title);
+      cy.get('#product-price-input').type(item.price);
+      cy.get('#product-price-input').type(item.quantity);
+      cy.get('#product-quantity-input').type(5).type('{enter}');
+
+      cy.get('.product-inventory tr').should('have.length', 2);
+      cy.get('.product-inventory tr')
+        .last()
+        .contains(item.title)
+        .siblings()
+        .contains(item.price)
+        .siblings()
+        .contains(item.quantity);
+    });
   });
 });
