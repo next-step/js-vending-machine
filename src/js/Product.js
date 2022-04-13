13dc16c5cb1c6a510c21bdcefcced89fcfa35dd4
circlegiven <circlegiven@gmail.com>
2022-04-13 15:40:52 +0900

    [상품관리] Product 클래스 추가

---
+++ src/js/Product.js
@@ -0,0 +1,66 @@
+export const MIN_PRICE = 100;
+export const MIN_QUANTITY = 1;
+export const STEP_PRICE = 10;
+
+function isEmpty(value) {
+  return value === undefined || value === null || value.trim() === '';
+}
+
+export default class Product {
+  #name;
+  #price;
+  #quantity;
+
+  constructor({ name, price, quantity }) {
+    Product.#validate({ name, price, quantity });
+    this.#name = name.trim();
+    this.#price = price;
+    this.#quantity = quantity;
+  }
+
+  get name() {
+    return this.#name;
+  }
+
+  get price() {
+    return this.#price;
+  }
+
+  get quantity() {
+    return this.#quantity;
+  }
+
+  static #validateName(name) {
+    if (isEmpty(name)) {
+      throw new Error('상품명은 필수값입니다.');
+    }
+  }
+
+  static #validatePrice(price) {
+    if (isEmpty(price)) {
+      throw new Error('가격은 필수값입니다.');
+    }
+    if (price < MIN_PRICE) {
+      throw new Error(`가격은 ${MIN_PRICE} 보다 커야됩니다.`);
+    }
+    if (price % STEP_PRICE !== 0) {
+      throw new Error(`가격은 ${STEP_PRICE} 단위여야 합니다.`);
+    }
+  }
+
+  static #validateQuantity(quantity) {
+    if (isEmpty(quantity)) {
+      throw new Error('수량은 필수값입니다.');
+    }
+
+    if (quantity < MIN_QUANTITY) {
+      throw new Error(`수량은 ${MIN_QUANTITY} 이상 이여야 합니다.`);
+    }
+  }
+
+  static #validate({ name, price, quantity }) {
+    Product.#validateName(name);
+    Product.#validatePrice(price);
+    Product.#validateQuantity(quantity);
+  }
+}
