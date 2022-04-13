2e2c45cc4bdfe703f63b67b6c1825b60056037f2
circlegiven <circlegiven@gmail.com>
2022-04-13 11:18:55 +0900

    [테스트] 상품관리 초기 랜더링 테스트

--- cypress/integration/vending-machine.spec.js
+++ cypress/integration/vending-machine.spec.js
@@ -7,6 +7,25 @@ Cypress.Commands.add('$vendingMachineManageMenu', () =>
 Cypress.Commands.add('$productPurchaseMenu', () =>
   cy.get('#product-purchase-menu')
 );
+Cypress.Commands.add('$app', () => cy.get('#app'));
+Cypress.Commands.add('$productContainer', () =>
+  cy.$app().get('div.product-container')
+);
+Cypress.Commands.add('$productInventory', () =>
+  cy.$app().get('table.product-inventory')
+);
+Cypress.Commands.add('$productNameInput', () =>
+  cy.$app().get('#product-name-input')
+);
+Cypress.Commands.add('$productPriceInput', () =>
+  cy.$app().get('#product-price-input')
+);
+Cypress.Commands.add('$productQuantityInput', () =>
+  cy.$app().get('#product-quantity-input')
+);
+Cypress.Commands.add('$productAddSubmit', () =>
+  cy.$app().get('#product-add-button')
+);
 
 describe('자판기', () => {
   beforeEach(() => {
@@ -18,6 +37,20 @@ describe('자판기', () => {
       cy.$productManageMenu().should('be.visible');
       cy.$vendingMachineManageMenu().should('be.visible');
       cy.$productPurchaseMenu().should('be.visible');
+    });
+  });
+
+  describe('상품관리 화면 테스트', () => {
+    it('상품명, 가격, 수량을 입력할 수 있는 폼이 보인다.', () => {
+      cy.$productContainer().should('be.visible');
+      cy.$productNameInput().should('be.visible');
+      cy.$productPriceInput().should('be.visible');
+      cy.$productQuantityInput().should('be.visible');
+      cy.$productAddSubmit().should('be.visible');
+    });
+
+    it('상품 목록이 보인다.', () => {
+      cy.$productInventory().should('be.visible');
     });
   });
 });
