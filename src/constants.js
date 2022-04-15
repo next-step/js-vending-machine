dcaf52ce0148782b26d877d10674b9f9eb3afa31
정현수 <jung660317@naver.com>
2022-04-15 16:03:37 +0900

    test: 메뉴 선택 테스트 생성

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,8 @@
 export const SELECTOR = Object.freeze({
   APP_ID: 'app',
+  PRODUCT_MANAGE_MENU_ID: 'product-manage-menu',
+  VENDING_MACHINE_MANAGE_MENU_ID: 'vending-machine-manage-menu',
+  PRODUCT_PURCHASE_MENU_ID: 'product-purchase-menu',
   PRODUCT_NAME_INPUT_ID: 'product-name-input',
   PRODUCT_PRICE_INPUT_ID: 'product-price-input',
   PRODUCT_QUANTITY_INPUT_ID: 'product-quantity-input',
