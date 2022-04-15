95277e32a56f1a5bd7c97c255c3d290ab3bcee18
정현수 <jung660317@naver.com>
2022-04-15 15:28:59 +0900

    chore: ProductManagerView selector 상수화 처리

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,8 @@
 export const SELECTOR = Object.freeze({
   APP_ID: 'app',
-  PRODUCT_MANAGER_CONTAINER_ID: 'product-manager-container',
+  PRODUCT_NAME_INPUT_ID: 'product-name-input',
+  PRODUCT_PRICE_INPUT_ID: 'product-price-input',
+  PRODUCT_QUANTITY_INPUT_ID: 'product-quantity-input',
+  PRODUCT_ADD_BUTTON_ID: 'product-add-button',
+  PRODUCT_INVENTORY_CONTAINER_ID: 'product-inventory-container',
 });
--- src/views/ProductManagerView.js
+++ src/views/ProductManagerView.js
@@ -1,3 +1,5 @@
+import { SELECTOR } from '../constants.js';
+
 class ProductManagerView {
   constructor(target) {
     this.$target = target;
@@ -12,10 +14,10 @@ class ProductManagerView {
     return String.raw`
       <h3>상품 추가하기</h3>
       <div class="product-container">
-        <input type="text" id="product-name-input" placeholder="상품명" />
-        <input type="number" id="product-price-input" placeholder="가격" />
-        <input type="number" id="product-quantity-input" placeholder="수량" />
-        <button id="product-add-button">추가하기</button>
+        <input type="text" id="${SELECTOR.PRODUCT_NAME_INPUT_ID}" placeholder="상품명" />
+        <input type="number" id="${SELECTOR.PRODUCT_PRICE_INPUT_ID}" placeholder="가격" />
+        <input type="number" id="${SELECTOR.PRODUCT_QUANTITY_INPUT_ID}" placeholder="수량" />
+        <button id="${SELECTOR.PRODUCT_ADD_BUTTON_ID}">추가하기</button>
       </div>
       <table class="product-inventory">
         <colgroup>
@@ -30,7 +32,7 @@ class ProductManagerView {
             <th>수량</th>
           </tr>
         </thead>
-        <tbody id="product-inventory-container"></tbody>
+        <tbody id="${SELECTOR.PRODUCT_INVENTORY_CONTAINER_ID}"></tbody>
       </table>
     `;
   }
