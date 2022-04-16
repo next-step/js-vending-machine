c1c10f41379b4fb9cc918e6bbc489a5ce7dcd263
Tene <tene@nextunicorn.kr>
2022-04-17 00:31:52 +0900

    🎆Style: 앱에서 사용할 상수 추가

---
+++ src/constants.js
@@ -0,0 +1,35 @@
+export const ACTION_KEY = {
+  INIT: 'INIT',
+  PULL: 'PULL',
+  PUSH: 'PUSH',
+};
+
+export const STORAGE_KEY = {
+  PRODUCT: 'PRODUCT',
+  CHARGE: 'CHARGE',
+  PURCHASE: 'PURCHASE',
+};
+
+export const COINS = {
+  500: 0,
+  100: 0,
+  50: 0,
+  10: 0,
+};
+
+export const INITIAL_STORE = {
+  [STORAGE_KEY.PRODUCT]: [],
+  [STORAGE_KEY.CHARGE]: COINS,
+};
+
+export const VENDING_MACHINE = {
+  MIN_PRICE: 100,
+  PRICE_STEP: 10,
+  MIN_QUANTITY: 1,
+  QUANTITY_STEP: 1,
+};
+
+export const ERROR_MESSAGE = {
+  INVALID_TYPE: '옳지 못한 타입입니다.',
+  NOT_EXISTS_KEY: '등록되지 않은 상태 키입니다.',
+};
