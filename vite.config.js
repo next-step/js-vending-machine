8302fb184e32817b947a0b93204c21895903c050
tolluset <dlqud19@gmail.com>
2022-04-17 00:22:38 +0900

    chore: :wrench: add settings

---
+++ vite.config.js
@@ -0,0 +1,18 @@
+import eslint from '@rollup/plugin-eslint';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+  base: './',
+  build: {
+    sourcemap: true,
+  },
+  plugins: [
+    {
+      ...eslint({
+        include: '*/**/*.js',
+      }),
+      enforce: 'pre',
+      apply: 'serve',
+    },
+  ],
+});
