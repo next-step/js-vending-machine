3e44235350064f54074bcad3fd8dfaa91860457c
dahye1013 <alrean@naver.com>
2022-04-16 20:36:32 +0900

    reafactor: 상품관리 설정 객체 interface 활용하여 readonly

--- src/ts/utils/config.ts
+++ src/ts/utils/config.ts
@@ -1,5 +1,9 @@
-export const PRODUCT_CONFIG = Object.freeze({
+interface ProductConfigContainer {
+  readonly [prop: string]: number;
+}
+
+export const PRODUCT_CONFIG: ProductConfigContainer = {
   MIN_PRICE: 100,
   MIN_QUANTITY: 1,
-  SHOULD_BE_DIVIDED: 10
-});
+  SHOULD_BE_DIVIDED: 10,
+};
