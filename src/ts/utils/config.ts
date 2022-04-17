49f5991a1b155e040e568aae5901a6cf1a0dadad
dahye1013 <alrean@naver.com>
2022-04-18 01:15:12 +0900

    refactor: 상수 선언 readonly 방식 수정

--- src/ts/utils/config.ts
+++ src/ts/utils/config.ts
@@ -1,9 +1,5 @@
-interface ProductConfigContainer {
-  readonly [prop: string]: number;
-}
-
-export const PRODUCT_CONFIG: ProductConfigContainer = {
+export const PRODUCT_CONFIG = {
   MIN_PRICE: 100,
   MIN_QUANTITY: 1,
   SHOULD_BE_DIVIDED: 10,
-};
+} as const;
--- src/ts/utils/message.ts
+++ src/ts/utils/message.ts
@@ -1,12 +1,10 @@
-interface MessageContainer {
-  readonly [prop: string]: string;
-}
+import { PRODUCT_CONFIG } from './config';
 
-export const ERROR: MessageContainer = {
-  LESS_THAN_MIN_PRICE: '상품 가격은 최소 100원 이상이어야 합니다!',
-  NOT_DIVIDED_PRICE: '상품 가격은 10원 단위로 나누어 떨어져야합니다!',
-  LESS_THAN_MIN_QUANTITY: '상품 가격은 10원 단위로 나누어 떨어져야합니다!',
+export const ERROR = {
+  LESS_THAN_MIN_PRICE: `상품 가격은 최소 ${PRODUCT_CONFIG.MIN_PRICE}원 이상이어야 합니다!`,
+  NOT_DIVIDED_PRICE: `상품 가격은 ${PRODUCT_CONFIG.SHOULD_BE_DIVIDED}원 단위로 나누어 떨어져야합니다!`,
+  LESS_THAN_MIN_QUANTITY: `상품 가격은 ${PRODUCT_CONFIG.MIN_QUANTITY}원 단위로 나누어 떨어져야합니다!`,
   NO_STORAGE_ITEM: '아이템이 존재하지 않습니다!',
   NO_PRODUCT_ITEM: '🎁 상품을 추가해주세요!🎁 ',
   FAIL_ADD_PRODUCT: '상품 등록에 실패했습니다. 😢',
-};
+} as const;
