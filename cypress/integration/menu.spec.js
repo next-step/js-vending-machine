b33b8f60b61f2998f586949daaca1b97ba586b8d
정현수 <jung660317@naver.com>
2022-04-17 20:31:12 +0900

    chore: 코드 간략화

--- cypress/integration/menu.spec.js
+++ cypress/integration/menu.spec.js
@@ -1,9 +1,7 @@
 import { SELECTOR } from '../../src/constants.js';
 
 describe('메뉴 선택 관련 테스트', () => {
-  beforeEach(() => {
-    cy.visit('/');
-  });
+  beforeEach(() => cy.visit('/'));
 
   context('각 메뉴 버튼을 클릭하면 해당 탭으로 이동합니다.', () => {
     it('상품 관리 탭을 클릭하면 상품 탭으로 이동합니다.', () => {
