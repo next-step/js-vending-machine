8fee83af136fa170e9a7992871c20cf624ab3881
tolluset <dlqud19@gmail.com>
2022-04-18 00:09:59 +0900

    chore: :test_tube: add data property for test

--- src/domains/products/management/components/ProductAppender.js
+++ src/domains/products/management/components/ProductAppender.js
@@ -1,8 +1,8 @@
 const ProductAppender = () => `
         <div class="product-container">
-            <input type="text" id="product-name-input" placeholder="상품명" />
-            <input type="number" id="product-price-input" placeholder="가격" />
-            <input type="number" id="product-quantity-input" placeholder="수량" />
+            <input type="text" id="product-name-input" placeholder="상품명" data-test="product-name-input"/>
+            <input type="number" id="product-price-input" placeholder="가격" data-test="product-price-input"/>
+            <input type="number" id="product-quantity-input" placeholder="수량" data-test="product-quantity-input"/>
             <button id="product-add-button">추가하기</button>
         </div>`;
 
