4bd5571244fd0eb9d0744a49e1999cc05e60f763
정현수 <jung660317@naver.com>
2022-04-17 21:24:16 +0900

    feat: 상품명에 대한 검증 구현
    - 상품명이 비어있을 때 경고창 띄움

--- src/VendingMachine.js
+++ src/VendingMachine.js
@@ -1,9 +1,21 @@
 import { ProductManageController, TabController } from './controllers/index.js';
 
+import { $ } from './utils/dom.js';
+import { SELECTOR } from './constants.js';
+
 class VendingMachine {
   constructor(target) {
     this.$productManageController = new ProductManageController(target);
     this.$tabController = new TabController(target);
+    this.initEvents();
+  }
+
+  initEvents() {
+    $(`#${SELECTOR.APP_ID}`).addEventListener('click', event => {
+      if (event.target.id === SELECTOR.PRODUCT_ADD_BUTTON_ID) {
+        this.$productManageController.addProduct();
+      }
+    });
   }
 }
 
--- src/controllers/ProductManageController.js
+++ src/controllers/ProductManageController.js
@@ -1,8 +1,24 @@
+import { ERROR_MESSAGE, SELECTOR } from '../constants.js';
+import { $ } from '../utils/dom.js';
 import { ProductManageView } from '../views/index.js';
 
 class ProductManageController {
   constructor(target) {
     this.$productManageView = new ProductManageView(target);
+  }
+
+  addProduct() {
+    const inputProductName = $(`#${SELECTOR.PRODUCT_NAME_INPUT_ID}`).value;
+
+    try {
+      this.validateProductName(inputProductName);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  validateProductName(productName) {
+    if (!productName) throw new Error(ERROR_MESSAGE.REQUIRED_PRODUCT_NAME);
   }
 }
 
