b8529ae99868365bd628147ed213777b4e8be7fd
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-04-17 18:10:51 +0900

    feat: constants 파일

---
+++ src/js/constants.js
@@ -0,0 +1,10 @@
+export const PRODUCT_PRICE_MINIMUM = 100;
+export const PRODUCT_NUMBER_MINIMUM = 1;
+
+export const EMPTY_PRODUCT_NAME = `상품 이름을 입력해주세요.`;
+export const EMPTY_PRODUCT_PRICE = `상품 가격을 입력해주세요.`;
+export const EMPTY_PRODUCT_NUMBER = `상품 수량을 입력해주세요.`;
+
+export const INVALID_PRODUCT_PRICE = `상품 가격은 10원으로 나누어져야 합니다.`;
+export const INVALID_PRODUCT_NUMBER_MINIMUM = `상품 수량은 최소 ${PRODUCT_NUMBER_MINIMUM}개 이상이여야 합니다.`;
+export const INVALID_PRODUCT_PRICE_MINIMUM = `상품 가격은 최소 ${PRODUCT_PRICE_MINIMUM}원 이상이여야 합니다.`;
