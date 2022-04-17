57c2a695e80ee8272dcb86ca69942375d170b13b
JeongBin0227 <45503248+JeongBin0227@users.noreply.github.com>
2022-04-17 18:13:45 +0900

    feat: VendingMachine 파일

---
+++ src/js/VendingMachine.js
@@ -0,0 +1,54 @@
+import { $, $$ } from './dom.js';
+
+import ProductMange from './section/ProductMange/index.js';
+import ProductPurchaseeManage from './section/ProductPurchaseManage/index.js';
+import VendingMachineManage from './section/VendingMachineManage/index.js';
+import actionMap from './eventAction.js';
+import store from './store/store.js';
+
+class VendingMachine {
+  constructor() {
+    this.$productManage = $('.product-manage');
+    this.$vendingMachineManage = $('.vending-machine-manage');
+    this.$productPurchaseManage = $('.product-purchase-manage');
+    this.$menuButtons = $$('.menu-button');
+
+    this.init();
+    this.setEvent();
+    this.render();
+  }
+
+  init() {
+    this.$productManage.innerHTML = '';
+    this.$vendingMachineManage.innerHTML = '';
+  }
+
+  setEvent() {
+    for (const menuButton of this.$menuButtons) {
+      menuButton.addEventListener('click', this.setMenu);
+    }
+  }
+
+  setMenu(event) {
+    const menu = event.target.dataset.menu;
+    actionMap?.SET_MENU(menu);
+  }
+
+  render() {
+    const { menu } = store.getState();
+    if (menu === 'productManage') {
+      ProductMange(this.$productManage);
+      return;
+    }
+    if (menu === 'vendingMachineManage') {
+      VendingMachineManage(this.$vendingMachineManage);
+      return;
+    }
+    if (menu === 'productPurchaseManage') {
+      ProductPurchaseeManage(this.$productPurchaseManage);
+      return;
+    }
+  }
+}
+
+export default VendingMachine;
