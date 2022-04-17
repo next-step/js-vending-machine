cc2894565714db5f424f3374ba4a8d170393e55a
Tene <tene@nextunicorn.kr>
2022-04-18 02:05:13 +0900

    🎆Style: prettier-ignore를 제거하고 tab을 space로 변경한다.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,20 @@ import './components/index.js';
 import { $element } from './helpers/index.js';
 import router from './routes.js';
 
-// prettier-ignore
 const AppTemplate = $element(/*html*/ `
-	<div id="app">
-		<header>
-			<div>
-				<h1>🏧자판기🏧</h1>
-			</div>
-			<nav>
-				<a href="#/product" id="product-manage-menu"><button>상품 관리</button></a>
-				<a href="#/charge" id="vending-machine-manage-menu"><button>잔돈 충전</button></a>
-				<a href="#/purchase" id="product-purchase-menu"><button>상품 구매</button></a>
-			</nav>
-		</header>
-		<main></main>
-	</div>`);
+  <div id="app">
+    <header>
+      <div>
+        <h1>🏧자판기🏧</h1>
+      </div>
+      <nav>
+        <a href="#/product" id="product-manage-menu"><button>상품 관리</button></a>
+        <a href="#/charge" id="vending-machine-manage-menu"><button>잔돈 충전</button></a>
+        <a href="#/purchase" id="product-purchase-menu"><button>상품 구매</button></a>
+      </nav>
+    </header>
+    <main></main>
+  </div>`);
 
 document.body.insertAdjacentElement('afterbegin', AppTemplate);
 
