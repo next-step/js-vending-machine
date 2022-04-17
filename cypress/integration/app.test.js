60e7553535d79d8ed85ae4e98b2c3e98c9361565
Tene <tene@nextunicorn.kr>
2022-04-18 01:49:43 +0900

    🧪Test: 스탭1 테스트 코드 추가 - 사용자 행위에 대해서만 테스트

--- cypress/integration/app.test.js
+++ cypress/integration/app.test.js
@@ -1,29 +1,116 @@
-const BASE_URL = '../../index.html';
+import { APP_KEY } from '../../src/constants.js';
 
-// TODO: https://glebbahmutov.com/blog/form-validation-in-cypress/
+const BASE_URL = '../../index.html';
 
 describe('Vending Machine', () => {
   beforeEach(() => {
     cy.visit(BASE_URL);
+    cy.initLocalStorage();
   });
 
   describe('각 탭에 최초 접속 시', () => {
-    it('상품 관리 탭 : 최초 상품 목록은 비워진 상태이다.', () => {});
-    it('잔돈 충전 탭 : 최초 자판기 보유 금액은 0원, 각 동전의 개수는 0개이다.', () => {});
-    it('상품 구매 탭 : 최초 충전 금액은 0원, 반환된 각 동전의 개수는 0개이다.', () => {});
+    it('로컬 스토리지는 초깃값으로 존재해야 한다.', () => {
+      expect(localStorage.getItem(APP_KEY)).to.be.equal('{"PRODUCT":[],"CHARGE":{"10":0,"50":0,"100":0,"500":0}}');
+    });
+    it('상품 관리 탭 : 최초 상품 목록은 비워진 상태이다.', () => {
+      cy.$('#product-manage-menu')
+        .click()
+        .then(() => {
+          expect(cy.$('tbody').children().should('have.length', 0));
+        });
+    });
+    it('잔돈 충전 탭 : 최초 자판기 보유 금액은 0원, 각 동전의 개수는 0개이다.', () => {
+      cy.$('#vending-machine-manage-menu')
+        .click()
+        .then(() => {
+          expect(cy.$('[data-charge-coin="500"]').should('have.text', 0));
+          expect(cy.$('[data-charge-coin="100"]').should('have.text', 0));
+          expect(cy.$('[data-charge-coin="50"]').should('have.text', 0));
+          expect(cy.$('[data-charge-coin="10"]').should('have.text', 0));
+        });
+    });
+    it('상품 구매 탭 : 최초 충전 금액은 0원, 반환된 각 동전의 개수는 0개이다.', () => {
+      cy.$('#product-purchase-menu')
+        .click()
+        .then(() => {
+          expect(cy.$('#purchase-charged-money').should('have.text', 0));
+
+          expect(cy.$('[data-return-coin="500"]').should('have.text', 0));
+          expect(cy.$('[data-return-coin="100"]').should('have.text', 0));
+          expect(cy.$('[data-return-coin="50"]').should('have.text', 0));
+          expect(cy.$('[data-return-coin="10"]').should('have.text', 0));
+        });
+    });
   });
 
   describe('상품 관리 탭에서 상품을 추가하는 경우', () => {
-    describe('입력값 유효성 검사 실패 시 경고창을 출력한다.', () => {
-      it(`공백은 허용되지 않는다.`, () => {});
-      it(`금액은 100원 미만으로 입력될 수 없다.`, () => {});
-      it(`금액의 단위는 10원이어야 한다.`, () => {});
-      it(`수량은 1 미만으로 입력될 수 없다.`, () => {});
-      it(`수량의 단위는 1이어야 한다.`, () => {});
+    describe('입력값 유효성 검사 실패 시 input이 invalid 된다.', () => {
+      it(`공백은 허용되지 않는다.`, () => {
+        cy.inputProduct({ name: '', price: '', quantity: '' }).then(() => {
+          cy.checkValidForm('.product-add-form', 3);
+        });
+      });
+      it(`금액은 100원 미만으로 입력될 수 없다.`, () => {
+        cy.inputProduct({ name: '콜라', price: 99, quantity: 2 }).then(() => {
+          cy.checkValidForm('.product-add-form', 1);
+        });
+      });
+      it(`금액의 단위는 10원이어야 한다.`, () => {
+        cy.inputProduct({ name: '콜라', price: 109, quantity: 2 }).then(() => {
+          cy.checkValidForm('.product-add-form', 1);
+        });
+      });
+      it(`수량은 1 미만으로 입력될 수 없다.`, () => {
+        cy.inputProduct({ name: '콜라', price: 100, quantity: -1 }).then(() => {
+          cy.checkValidForm('.product-add-form', 1);
+        });
+      });
+      it(`수량의 단위는 1이어야 한다.`, () => {
+        cy.inputProduct({ name: '콜라', price: 100, quantity: 1.1 }).then(() => {
+          cy.checkValidForm('.product-add-form', 1);
+        });
+      });
     });
 
     describe('입력값 유효성 검사에 성공하는 경우', () => {
-      it(`같은 상품명이 입력되면 새로운 정보로 대체(Replace)된다.`, () => {});
+      it(`상품 테이블에 상품 정보가 추가된다.`, () => {
+        const product = { name: '콜라', price: 1500, quantity: 10 };
+        cy.inputProduct(product);
+
+        cy.$('.product-add-form')
+          .submit()
+          .then(() => {
+            cy.$('tbody tr:first-child')
+              .find('td')
+              .eq(0)
+              .should('have.text', product.name)
+              .next()
+              .should('have.text', product.price)
+              .next()
+              .should('have.text', product.quantity);
+          });
+      });
+      it(`같은 상품명이 입력되면 새로운 정보로 대체(Replace)된다.`, () => {
+        cy.inputProduct({ name: '콜라', price: 1500, quantity: 5 });
+        cy.$('.product-add-form').submit();
+
+        cy.inputProduct({ name: '사이다', price: 2500, quantity: 10 });
+        cy.$('.product-add-form').submit();
+
+        cy.inputProduct({ name: '콜라', price: 3500, quantity: 12 });
+        cy.$('.product-add-form')
+          .submit()
+          .then(() => {
+            cy.$('tbody tr:first-child')
+              .find('td')
+              .eq(0)
+              .should('have.text', '콜라')
+              .next()
+              .should('have.text', 3500)
+              .next()
+              .should('have.text', 12);
+          });
+      });
     });
   });
 
