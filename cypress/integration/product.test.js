570c2e2f0c45426e60e64d27baaa3f69586a9e46
hdpyo <hudongpyo.dev@gmail.com>
2022-04-19 14:36:23 +0900

    test: '상품 추가하기' 영역이 보여야 한다.

--- cypress/integration/product.test.js
+++ cypress/integration/product.test.js
@@ -2,4 +2,12 @@ describe('Product', () => {
   beforeEach(() => {
     cy.visit('./index.html');
   });
+
+  it('[상품 추가하기] 영역이 보여야 한다.', () => {
+    // todo : 이름, 가격, 수량 input과 추가하기 버튼이 있어야 한다.
+    cy.get('#product-name-input').should('be.visible');
+    cy.get('#product-price-input').should('be.visible');
+    cy.get('#product-quantity-input').should('be.visible');
+    cy.get('#product-add-button').should('be.visible');
+  });
 });
