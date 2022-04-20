eb710bb380ea72e84b71c6c54cf025a1d258d000
hdpyo <hudongpyo.dev@gmail.com>
2022-04-20 17:53:59 +0900

    chore: 불필요한 주석 제거

--- cypress/integration/product.test.js
+++ cypress/integration/product.test.js
@@ -5,7 +5,6 @@ describe('Product', () => {
   });
 
   it('[상품 추가하기] 영역이 보여야 한다.', () => {
-    // todo : 이름, 가격, 수량 input과 추가하기 버튼이 있어야 한다.
     cy.get('#product-name-input').should('be.visible');
     cy.get('#product-price-input').should('be.visible');
     cy.get('#product-quantity-input').should('be.visible');
