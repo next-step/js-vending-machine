671110ac01add429757319494196118815fcb659
정현수 <jung660317@naver.com>
2022-04-20 21:38:31 +0900

    refactor: VendingMachineView 리팩토링

--- src/VendingMachineView.js
+++ src/VendingMachineView.js
@@ -1,22 +1,32 @@
-import { TAB } from './constants.js';
 import {
-  ProductManageTemplate,
-  VendingMachineManageTemplate,
-  ProductPurchaseTemplate,
+  productManageTabTemplate,
+  productTemplate,
+  productPurchaseTabTemplate,
+  vendingMachineManageTabTemplate,
 } from './templates/index.js';
+
+import { SELECTOR, TAB } from './constants.js';
+import { $ } from './utils/dom.js';
+
+const RenderTab = {
+  [TAB.PRODUCT_MANAGE_TAB]: () => productManageTabTemplate(),
+  [TAB.PRODUCT_PURCHASE_TAB]: () => productPurchaseTabTemplate(),
+  [TAB.VENDING_MACHINE_MANAGE_TAB]: () => vendingMachineManageTabTemplate(),
+};
 
 class VendingMachineView {
   constructor(target) {
     this.target = target;
-    this.changeTab = {
-      [TAB.PRODUCT_MANAGE_TAB]: state => ProductManageTemplate(state),
-      [TAB.PRODUCT_PURCHASE_TAB]: state => VendingMachineManageTemplate(state),
-      [TAB.VENDING_MACHINE_MANAGE_TAB]: state => ProductPurchaseTemplate(state),
-    };
   }
 
-  renderTab(state) {
-    this.target.innerHTML = this.changeTab[state.currentTab](state);
+  renderTab(changeTab) {
+    this.target.innerHTML = RenderTab[changeTab]();
+  }
+
+  renderProductTableInProductManageTab(products) {
+    $(`#${SELECTOR.PRODUCT_INVENTORY_CONTAINER_ID}`).innerHTML = products
+      .map(({ name, price, quantity }) => productTemplate({ name, price, quantity }))
+      .join('');
   }
 }
 
