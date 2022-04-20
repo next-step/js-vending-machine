3247e13fce04ecbd44786d19a2728cc1b67900a7
Minsiki <43837214+Minsiki@users.noreply.github.com>
2022-04-21 01:55:19 +0900

    Exception Class 수정
    static method로 구성되어있던 class를 객체 형태로 수정

--- src/js/exception/ProductException.js
+++ src/js/exception/ProductException.js
@@ -1,19 +1,19 @@
 import Product from "../domain/Product.js";
 
-export default class ProductException {
-    static notExistProductName() {
+export const ProductException = {
+    notExistProductName() {
         return new Error("상품명을 입력해주세요.");
-    }
+    },
 
-    static outOfRangePrice() {
+    outOfRangePrice() {
         return new Error(`상품 금액은 ${Product.PRICE_MIN}원이 최소 금액입니다.`);
-    }
+    },
 
-    static notMatchPriceUnit() {
+    notMatchPriceUnit() {
         return new Error(`상품 금액은 ${Product.PRICE_UNIT}원 단위로 입력해야 합니다.`);
-    }
+    },
 
-    static outOfRangeQuantity() {
+    outOfRangeQuantity() {
         return new Error(`상품 수량은 ${Product.QUANTITY_MIN}개 이상 입력해야 합니다.`);
-    }
-}
+    },
+};
