3834107c4f9672c76de55f949617c6143ee59692
hdpyo <hudongpyo.dev@gmail.com>
2022-04-20 18:32:09 +0900

    refactor: InputValidation 리팩토링
    - if문을 Guard Clause 방식으로 수정

--- src/js/InputValidation.js
+++ src/js/InputValidation.js
@@ -1,33 +1,21 @@
 const InputValidation = {
   isEmptyProductInput(name, price, quantity) {
-    if (name === '' || price === '' || quantity === '') {
-      return false;
-    }
-
+    if (name === '' || price === '' || quantity === '') return false;
     return true;
   },
 
   isUnder100Price(price) {
-    if (price < 100) {
-      return false;
-    }
-
+    if (price < 100) return false;
     return true;
   },
 
   isPriceInUnitsOf10Won(price) {
-    if (price % 10 === 0) {
-      return true;
-    }
-
+    if (price % 10 === 0) return true;
     return false;
   },
 
   isUnderMinQuantity(quantity) {
-    if (quantity < 1) {
-      return false;
-    }
-
+    if (quantity < 1) return false;
     return true;
   },
 };
