e1a095e354dbd252c95895573a5789555bd5f17e
Raven <61782746+Blackraven93@users.noreply.github.com>
2022-04-20 10:18:24 +0900

    Test: web component화에 따른 테스트코드 변경

--- cypress/integration/vendingMachine.spec.js
+++ cypress/integration/vendingMachine.spec.js
@@ -1,5 +1,5 @@
 import { VALIDATE } from '../../js/util/consts.js';
-
+import './selector.js';
 // MEMO: 테스트코드 작성 시 상수 값 분리 및 중복체크
 describe('vending machine', () => {
   beforeEach(() => {
@@ -8,48 +8,29 @@ describe('vending machine', () => {
 
   // COMPLETE
   describe('상품관리 화면', () => {
-    Cypress.Commands.add('addProduct', () => {
-      cy.get('product-handling-board')
-        .shadow()
-        .find('[data-product=name-input]')
-        .type('아메리카노');
-      cy.get('product-handling-board')
-        .shadow()
-        .find('[data-product=price-input]')
-        .type('4000');
-      cy.get('product-handling-board')
-        .shadow()
-        .find('[data-product=quantity-input]')
-        .type(`4{enter}`);
+    // COMPLETE
+    const product = ['아메리카노', '4000', '4'];
+
+    it('최초 상품 공백 확인', () => {
+      cy.findProduct().should('be.empty');
     });
 
     // COMPLETE
-    it('최초 상품 공백 확인', () => {
-      cy.get('product-handling-board')
-        .shadow()
-        .find('product-inventory')
-        .shadow()
-        .find('[data-manage=product]')
-        .should('be.empty');
-    });
-
-    // FIXME: Shadow dom에 따른 테그 수정
     it('상품명, 금액, 수량을 추가할 수 있다. 추가한 상품은 하위 UI에 반영된다.', () => {
-      const data = ['아메리카노', '4000', '4'];
-      cy.addProduct();
-      data.forEach((value) => {
-        cy.get('product-inventory')
+      cy.addProduct(...product);
+      product.forEach((value) => {
+        cy.findProductInventory()
           .shadow()
           .find('td')
           .should('contain.text', value);
       });
     });
 
-    // FIXME: Shadow dom에 따른 테그 수정
+    // COMPLETE
     it('상품 정보 공백처리', () => {
       const alert = cy.stub();
       cy.on('window:alert', alert);
-      cy.get('[data-product=add-button]')
+      cy.findAddButton()
         .click()
         .then((_) => {
           const message = alert.getCall(0).lastArg;
@@ -57,14 +38,13 @@ describe('vending machine', () => {
         });
     });
 
-    // FIXME: Shadow dom에 따른 테그 수정
+    // COMPLETE
     it('상품의 최소 가격은 100원이며, 10원으로 나누어 떨어져야 한다.', () => {
       const alert = cy.stub();
+      const priceUnitErrorData = ['아메리카노', '5', '4'];
       cy.on('window:alert', alert);
-      cy.get('[data-product=name-input]').type('아메리카노');
-      cy.get('[data-product=price-input]').type('5');
-      cy.get('[data-product=quantity-input]').type(`4{enter}`);
-      cy.get('[data-product=add-button]')
+      cy.addProduct(...priceUnitErrorData);
+      cy.findAddButton()
         .click()
         .then((_) => {
           const message = alert.getCall(0).lastArg;
@@ -72,16 +52,13 @@ describe('vending machine', () => {
         });
     });
 
-    // FIXME: Shadow dom에 따른 테그 수정
+    // COMPLETE
     it('같은 상품명의 데이터를 추가하면 기존의 상품에 해당하는 데이터는 새로운 상품으로 대체된다.', () => {
-      cy.addProduct();
-      cy.get('[data-product=name-input]').type('아메리카노');
-      cy.get('[data-product=price-input]').type('5000');
-      cy.get('[data-product=quantity-input]').type(`3{enter}`);
-
-      const data = ['아메리카노', '5000', '3'];
-      data.forEach((value) => {
-        cy.get('product-inventory')
+      const overLapProduct = ['아메리카노', '3000', '5'];
+      cy.addProduct(...product);
+      cy.addProduct(...overLapProduct);
+      overLapProduct.forEach((value) => {
+        cy.findProductInventory()
           .shadow()
           .find('td')
           .should('contain.text', value);
