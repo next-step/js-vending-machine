1d1df1acd10e22fe122df076350caa152baf286a
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-21 23:44:14 +0900

    🚧  feat: 상품 등록 기능 추가

---
+++ js/product/helper/product.js
@@ -0,0 +1,29 @@
+import { now } from '../../utils/date.js';
+
+export const RANGE = {
+  MAX_PRICE: 500_000,
+  MIN_PRICE: 10,
+  MAX_COUNT: 5_000,
+  MIN_COUNT: 1,
+};
+
+export const addProduct = (newProduct, prevProducts) =>
+  sortByRegisterTime(mergeProducts(newProduct, prevProducts));
+
+const getProductWithRegisterTime = (newProduct, prevProducts) => {
+  const isUniq = !prevProducts.some(({ name }) => name === newProduct.name);
+
+  if (isUniq) {
+    newProduct.registerTime = now();
+  }
+
+  return newProduct;
+};
+
+const mergeProducts = (newProduct, prevProducts) => {
+  const filtered = prevProducts.filter(({ name }) => name !== newProduct.name);
+  return filtered.concat(getProductWithRegisterTime(newProduct, prevProducts));
+};
+
+const sortByRegisterTime = (products) =>
+  products.sort(({ registerTime: a }, { registerTime: b }) => b - a);
---
+++ js/product/view/Product.js
@@ -0,0 +1,75 @@
+import { createFragmentWithTemplate, $ } from '../../utils/dom.js';
+import { addProduct, RANGE } from '../helper/product.js';
+
+const Product = ($app, store) => {
+  const $frag = createFragmentWithTemplate(productTemplate(RANGE));
+
+  const $form = $('form', $frag);
+  const $productList = $('#product-inventory-container', $frag);
+
+  const handleProductSubmit = (event) => {
+    event.preventDefault();
+
+    const newProducts = addProduct(
+      Object.fromEntries(new FormData($form)),
+      store.getState('products')
+    );
+
+    store.dispatch({
+      products: newProducts,
+    });
+  };
+
+  const renderProductList = ({ products }) => {
+    if (!products) return;
+    $productList.innerHTML = products.map(productRow).join('');
+  };
+
+  store.subscribe(renderProductList);
+
+  $form.addEventListener('submit', handleProductSubmit);
+
+  store.getState('products') && renderProductList(store.getState('products'));
+
+  return {
+    $app,
+    $frag,
+  };
+};
+
+const productTemplate = ({ MAX_PRICE, MIN_PRICE, MAX_COUNT, MIN_COUNT }) => `
+  <h3>상품 추가하기</h3>
+  <form class="product-container">
+    <fieldset>
+      <input required="true" type="text" id="product-name-input" name="name" placeholder="상품명" />
+      <input required="true" type="number" id="product-price-input" name="price" placeholder="가격" min=${MIN_PRICE} max=${MAX_PRICE} />
+      <input required="true" type="number" id="product-quantity-input" name="count" placeholder="수량" min=${MIN_COUNT} max=${MAX_COUNT} />
+      <button type="submit" id="product-add-button">추가하기</button>
+    </fieldset>
+  </form>
+  <table class="product-inventory">
+    <colgroup>
+      <col style="width: 140px" />
+      <col style="width: 100px" />
+      <col style="width: 100px" />
+    </colgroup>
+    <thead>
+      <tr>
+        <th>상품명</th>
+        <th>가격</th>
+        <th>수량</th>
+      </tr>
+    </thead>
+    <tbody id="product-inventory-container"></tbody>
+  </table>
+`;
+
+const productRow = ({ name, price, count }) => `
+  <tr>
+    <td>${name}</td>
+    <td>${price}</td>
+    <td>${count}</td>
+  </tr>
+`;
+
+export default Product;
