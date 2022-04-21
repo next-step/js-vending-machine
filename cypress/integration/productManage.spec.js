0c28e34ca560ef19e6a982f6f6584869c4e07429
정현수 <jung660317@naver.com>
2022-04-21 15:18:17 +0900

    test: alert 커맨드 생성

--- cypress/integration/productManage.spec.js
+++ cypress/integration/productManage.spec.js
@@ -7,94 +7,58 @@ describe('상품 추가하기 탭 관련 테스트', () => {
 
   context('상품명이 알맞게 입력되어야 합니다.', () => {
     it('상품명은 필수값 입니다.', () => {
-      const alertStub = cy.stub();
-      cy.on('window:alert', alertStub);
-
       cy.get(`#${SELECTOR.PRODUCT_PRICE_INPUT_ID}`).type(1000);
       cy.get(`#${SELECTOR.PRODUCT_QUANTITY_INPUT_ID}`).type(5);
-      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`)
-        .click()
-        .then(() => {
-          expect(alertStub.getCall(0)).to.be.calledWith(ERROR_MESSAGE.REQUIRED_PRODUCT_NAME);
-        });
+      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`).click();
+
+      cy.expectAlertWithMessage(ERROR_MESSAGE.REQUIRED_PRODUCT_NAME);
     });
   });
 
   context('상품 가격이 알맞게 입력되어야 합니다.', () => {
     it('상품 가격은 필수값 입니다.', () => {
-      const alertStub = cy.stub();
-      cy.on('window:alert', alertStub);
-
       cy.get(`#${SELECTOR.PRODUCT_NAME_INPUT_ID}`).type('apple');
       cy.get(`#${SELECTOR.PRODUCT_QUANTITY_INPUT_ID}`).type(5);
-      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`)
-        .click()
-        .then(() => {
-          expect(alertStub.getCall(0)).to.be.calledWith(ERROR_MESSAGE.REQUIRED_PRODUCT_PRICE);
-        });
+      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`).click();
+
+      cy.expectAlertWithMessage(ERROR_MESSAGE.REQUIRED_PRODUCT_PRICE);
     });
 
     it(`상품 가격은 ${STANDARD.PRODUCT_PRICE_MINIMUM}이상 정수여야 합니다.`, () => {
-      const alertStub = cy.stub();
-      cy.on('window:alert', alertStub);
-
       cy.get(`#${SELECTOR.PRODUCT_NAME_INPUT_ID}`).type('apple');
       cy.get(`#${SELECTOR.PRODUCT_PRICE_INPUT_ID}`).type(10);
       cy.get(`#${SELECTOR.PRODUCT_QUANTITY_INPUT_ID}`).type(5);
-      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`)
-        .click()
-        .then(() => {
-          expect(alertStub.getCall(0)).to.be.calledWith(
-            ERROR_MESSAGE.PRODUCT_PRICE_HAVE_TO_OVER_100,
-          );
-        });
+      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`).click();
+
+      cy.expectAlertWithMessage(ERROR_MESSAGE.PRODUCT_PRICE_HAVE_TO_OVER_100);
     });
 
     it(`상품 가격은 ${STANDARD.PRODUCT_PRICE_DIVIDE_BY}으로 나누어 떨어져야 합니다.`, () => {
-      const alertStub = cy.stub();
-      cy.on('window:alert', alertStub);
-
       cy.get(`#${SELECTOR.PRODUCT_NAME_INPUT_ID}`).type('apple');
       cy.get(`#${SELECTOR.PRODUCT_PRICE_INPUT_ID}`).type(999);
       cy.get(`#${SELECTOR.PRODUCT_QUANTITY_INPUT_ID}`).type(5);
-      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`)
-        .click()
-        .then(() => {
-          expect(alertStub.getCall(0)).to.be.calledWith(
-            ERROR_MESSAGE.PRODUCT_PRICE_HAVE_TO_DIVIDED_BY_10,
-          );
-        });
+      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`).click();
+
+      cy.expectAlertWithMessage(ERROR_MESSAGE.PRODUCT_PRICE_HAVE_TO_DIVIDED_BY_10);
     });
   });
 
   context('상품 수량이 알맞게 입력되어야 합니다.', () => {
     it('상품 수량은 필수값 입니다.', () => {
-      const alertStub = cy.stub();
-      cy.on('window:alert', alertStub);
-
       cy.get(`#${SELECTOR.PRODUCT_NAME_INPUT_ID}`).type('hi');
       cy.get(`#${SELECTOR.PRODUCT_PRICE_INPUT_ID}`).type(1000);
-      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`)
-        .click()
-        .then(() => {
-          expect(alertStub.getCall(0)).to.be.calledWith(ERROR_MESSAGE.REQUIRED_PRODUCT_QUANTITY);
-        });
+      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`).click();
+
+      cy.expectAlertWithMessage(ERROR_MESSAGE.REQUIRED_PRODUCT_QUANTITY);
     });
 
     it(`상품 수량은 ${STANDARD.PRODUCT_QUANTITY_MINIMUM}이상 정수여야 합니다.`, () => {
-      const alertStub = cy.stub();
-      cy.on('window:alert', alertStub);
-
       cy.get(`#${SELECTOR.PRODUCT_NAME_INPUT_ID}`).type('hi');
       cy.get(`#${SELECTOR.PRODUCT_PRICE_INPUT_ID}`).type(1000);
       cy.get(`#${SELECTOR.PRODUCT_QUANTITY_INPUT_ID}`).type(-1);
-      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`)
-        .click()
-        .then(() => {
-          expect(alertStub.getCall(0)).to.be.calledWith(
-            ERROR_MESSAGE.PRODUCT_QUANTITY_HAVE_TO_OVER_1,
-          );
-        });
+      cy.get(`#${SELECTOR.PRODUCT_ADD_BUTTON_ID}`).click();
+
+      cy.expectAlertWithMessage(ERROR_MESSAGE.PRODUCT_QUANTITY_HAVE_TO_OVER_1);
     });
   });
 
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,14 +1,3 @@
-// ***********************************************
-// This example commands.js shows you how to
-// create various custom commands and overwrite
-// existing commands.
-//
-// For more comprehensive examples of custom
-// commands please read more here:
-// https://on.cypress.io/custom-commands
-// ***********************************************
-//
-//
 // -- This is a parent command --
 // Cypress.Commands.add('login', (email, password) => { ... })
 //
@@ -23,3 +12,9 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add('expectAlertWithMessage', message => {
+  cy.on('window:alert', text => {
+    expect(text).to.contains(message);
+  });
+});
