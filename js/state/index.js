88e5d32848effb1032d493e32df79e0fe814b902
bomee.yoon <bomee.yoon@godo.co.kr>
2022-04-21 23:42:29 +0900

    🚧  feat: store 추가

---
+++ js/state/index.js
@@ -0,0 +1,32 @@
+import createStorage from '../utils/storage.js';
+
+const createStore = (initialState) => {
+  const storage = createStorage(initialState);
+
+  const listeners = [];
+
+  const getState = (key) =>
+    key ? { [key]: storage.get(key) } : storage.getAll();
+
+  const dispatch = (newState) => {
+    const prevState = storage.getAll();
+    storage.set({
+      ...prevState,
+      ...newState,
+    });
+
+    listeners.forEach((listener) => listener(storage.getAll()));
+  };
+
+  const subscribe = (listener) => {
+    listeners.push(listener);
+  };
+
+  return {
+    getState,
+    dispatch,
+    subscribe,
+  };
+};
+
+export default createStore;
