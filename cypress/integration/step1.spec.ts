4541630240807ea40c7e607af23674f48f3e9ac1
padawanR0k <padawanR0k@gmail.com>
2022-04-23 00:31:46 +0900

    step1-5 기능 구현

--- cypress/integration/step1.spec.ts
+++ cypress/integration/step1.spec.ts
@@ -59,19 +59,42 @@ describe('step1', () => {
 
     // given
     describe('이름의 상품이 있을 때' , () => {
-      before(() => reset())
+      before(() => {
+        reset();
+      })
 
       // when
       context('같은 상품명의 데이터를 추가하면', () => {
+        before(() => {
+          cy.get('#product-name-input').type('상품1')
+          cy.get('#product-price-input').type('100')
+          cy.get('#product-quantity-input').type('1')
+
+          cy.get('#product-add-button').click()
+          cy.get('#product-name-input').clear()
+          cy.get('#product-price-input').clear()
+          cy.get('#product-quantity-input').clear()
+
+          cy.get('#product-name-input').type('상품1')
+          cy.get('#product-price-input').type('200')
+          cy.get('#product-quantity-input').type('2')
+
+          cy.get('#product-add-button').click()
+        })
 
         // then
         it('step1-5 기존의 상품에 해당하는 데이터는 새로운 상품으로 대체된다.', () => {
-
+          expect(cy.get('#product-manage-name').should('have.text', '상품1'))
+          expect(cy.get('#product-manage-price').should('have.text', '200'))
+          expect(cy.get('#product-manage-quantity').should('have.text', '2'))
         })
       })
     })
 
     describe('사용자는 추가한 상품을 확인할 수 있다.', () => {
+      before(() => {
+        reset();
+      })
       it('step1-6 상품의 이름, 가격, 수량 순으로 상품 목록이 보여진다.', () => {
         cy.get('#product-name-input').type('상품1')
         cy.get('#product-price-input').type('100')
