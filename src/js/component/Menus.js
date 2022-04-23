559bd85a32864498e2974ee418c69c72af1abebf
Minsiki <pro.mskim0215@gmail.com>
2022-04-23 16:52:57 +0900

    이벤트 위임 및 선택된 메뉴 표시

--- src/js/component/Menus.js
+++ src/js/component/Menus.js
@@ -3,20 +3,48 @@ export default class Menus {
     static VENDING_MACHINE_MANAGE = "vending-machine-manage-menu";
     static PRODUCT_PURCHASE = "product-purchase-menu";
 
+    props;
+    selectedMenu;
+
     constructor(props) {
         this.props = props;
         this.mounted();
+
+        
+        this.#setSelectedMenu(Menus.PRODUCT_MANAGE);
     }
 
     mounted() {
-        document.querySelector(`#${Menus.PRODUCT_MANAGE}`).addEventListener("click", this.props.onProductManage.bind());
-        document
-            .querySelector(`#${Menus.VENDING_MACHINE_MANAGE}`)
-            .addEventListener("click", this.props.onVendingMachineManage.bind());
-        document
-            .querySelector(`#${Menus.PRODUCT_PURCHASE}`)
-            .addEventListener("click", this.props.onProductPurchase.bind());
+        document.querySelector("#menu-button-area").addEventListener("click", (event) => this.#onClickButton(event))
     }
 
-    setSelectedMenu(menu) {}
+    #onClickButton(event) {
+        switch(event.target.id) {
+            case Menus.PRODUCT_MANAGE: {
+                this.#setSelectedMenu(event.target.id);
+                this.props.onProductManage();
+                break;
+            }
+
+            case Menus.VENDING_MACHINE_MANAGE: {
+                this.#setSelectedMenu(event.target.id);
+                this.props.onVendingMachineManage();
+                break;
+            }
+
+            case Menus.PRODUCT_PURCHASE: {
+                this.#setSelectedMenu(event.target.id);
+                this.props.onProductPurchase();
+                break;
+            }
+        }
+    }
+
+    #setSelectedMenu(event) { 
+        if(!this.selectedMenu) this.selectedMenu = Menus.PRODUCT_MANAGE;
+
+        document.querySelector(`#${this.selectedMenu}`).classList.remove("selected");
+        this.selectedMenu = event;
+        document.querySelector(`#${this.selectedMenu}`).classList.add("selected");
+    }
 }
