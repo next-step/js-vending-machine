aa776b2d65c5a277868001f97d3f47ed5b2a8d98
정현수 <jung660317@naver.com>
2022-04-25 01:01:47 +0900

    feat: 충전 금액 검증 구현
    - 100 미만이면 경고창 띄우도록 구현
    - 10으로 나누어 떨어지지 않으면 경고창 띄우도록 구현

--- cypress/integration/vendingMachineManage.spec.js
+++ cypress/integration/vendingMachineManage.spec.js
@@ -1,17 +1,20 @@
 import { SELECTOR, ERROR_MESSAGE } from '../../src/constants.js';
 
 describe('잔돈 충전 탭 관련 테스트', () => {
-  beforeEach(() => cy.visit('/'));
+  beforeEach(() => {
+    cy.visit('/');
+    cy.get(`#${SELECTOR.VENDING_MACHINE_MANAGE_MENU_ID}`).click();
+  });
 
   context('잔돈 충전 금액은 알맞게 입력되어야 합니다.', () => {
     it('잔돈 충전 금액은 100원 이상이어야 합니다.', () => {
-      cy.get(`#${SELECTOR.VENDING_MACHINE_CHARGE_INPUT_ID}`).input('90');
+      cy.get(`#${SELECTOR.VENDING_MACHINE_CHARGE_INPUT_ID}`).type('90');
       cy.get(`#${SELECTOR.VENDING_MACHINE_CHARGE_BUTTON_ID}`).click();
       cy.expectAlertWithMessage(ERROR_MESSAGE.CHARGE_INPUT_HAVE_TO_OVER_100);
     });
 
     it('잔돈 충전 금액은 10원으로 나누어 떨어져야 합니다.', () => {
-      cy.get(`#${SELECTOR.VENDING_MACHINE_CHARGE_INPUT_ID}`).input('115');
+      cy.get(`#${SELECTOR.VENDING_MACHINE_CHARGE_INPUT_ID}`).type('115');
       cy.get(`#${SELECTOR.VENDING_MACHINE_CHARGE_BUTTON_ID}`).click();
       cy.expectAlertWithMessage(ERROR_MESSAGE.CHARGE_INPUT_HAVE_TO_DIVIDED_BY_10);
     });
--- src/controllers/VendingMachineManageController.js
+++ src/controllers/VendingMachineManageController.js
@@ -1,8 +1,26 @@
 import { VendingMachineManageView } from '../views/index.js';
 
+import { $ } from '../utils/dom.js';
+import { ERROR_MESSAGE, SELECTOR, STANDARD } from '../constants.js';
+
 class VendingMachineManageController {
   render() {
     VendingMachineManageView.render();
+  }
+
+  chargeCoin() {
+    const chargeMoney = $(`#${SELECTOR.VENDING_MACHINE_CHARGE_INPUT_ID}`).value;
+
+    try {
+      this.validateChargeMoney(chargeMoney);
+    } catch (error) {
+      alert(error.message);
+    }
+  }
+
+  validateChargeMoney(money) {
+    if (money < STANDARD.CHARGE_INPUT_MINIMUM) throw new Error(ERROR_MESSAGE.CHARGE_INPUT_HAVE_TO_OVER_100);
+    if (money % STANDARD.CHARGE_INPUT_DIVIDE_BY) throw new Error(ERROR_MESSAGE.CHARGE_INPUT_HAVE_TO_DIVIDED_BY_10);
   }
 }
 
