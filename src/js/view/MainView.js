990fc5f9045c569038eeb929407afecea3086e71
circlegiven <circlegiven@gmail.com>
2022-04-25 17:02:09 +0900

    [메인화면] 잔동충전 클릭시 화면 변경 추가

--- src/js/view/MainView.js
+++ src/js/view/MainView.js
@@ -1,11 +1,19 @@
 import ProductManageView from './ProductManageView.js';
+import VendingMachineManageView from './VendingMachineManageView.js';
 import { FORM } from '../constants/content-constant.js';
 
 const $app = document.querySelector('#app');
 const $productManageMenuSubmit = document.getElementById('product-manage-menu');
+const $vendingMachineManageMenuSubmit = document.getElementById(
+  'vending-machine-manage-menu'
+);
 
 function isProductManageContent(formId) {
   return formId === FORM.PRODUCT;
+}
+
+function isVendingMachineManageContent(formId) {
+  return formId === FORM.VENDING_MACHINE;
 }
 
 function changeAppContents(template) {
@@ -17,10 +25,20 @@ function handleProductManageMenu() {
   ProductManageView.initialize();
 }
 
+function handleVendingMachineManageMenu() {
+  changeAppContents(VendingMachineManageView.contents());
+  VendingMachineManageView.initialize();
+}
+
 function handleMenuContentSubmit(event) {
   const formId = event.target.id;
   if (isProductManageContent(formId)) {
     ProductManageView.handleProductAdd(event);
+    return;
+  }
+
+  if (isVendingMachineManageContent(formId)) {
+    VendingMachineManageView.handleChargingCoin(event);
   }
 }
 
@@ -36,6 +54,10 @@ function preventEmptyKeypress(event) {
 
 function eventBindings() {
   $productManageMenuSubmit.addEventListener('click', handleProductManageMenu);
+  $vendingMachineManageMenuSubmit.addEventListener(
+    'click',
+    handleVendingMachineManageMenu
+  );
   $app.addEventListener('submit', handleMenuContentSubmit);
   $app.addEventListener('keypress', preventEmptyKeypress);
 }
