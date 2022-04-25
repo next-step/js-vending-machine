9837c397269248eaca3e8413f4bd977b92ac881b
dahye1013 <alrean@naver.com>
2022-04-26 00:17:17 +0900

    refactor: path 관련 임시변수 제거

--- src/ts/router/router.ts
+++ src/ts/router/router.ts
@@ -1,27 +1,27 @@
 import { PageList, PAGE } from './pages';
 import * as model from '../model';
 
-const route = function (): void {
-  let [, path] = location.hash.split('#');
-  if (!path) path = PAGE.products.path;
 
+const route = function (): void {
+  const path = location.hash.substring(1) || PAGE.products.path;
   const currentView = PageList.find(page => path === page.path);
+
   document.querySelectorAll(`a[href]`)?.forEach(el => el.classList.remove('active'));
   document.querySelector(`a[href="${path}"]`)?.classList.add('active');
 
-    try {
-      const data = model.loadData(path);
+  try {
+    const data = model.loadData(path);
 
-      if (currentView) currentView.view.render(data);
-    } catch (err: Error | unknown) {
-      if (currentView && err instanceof Error) {
-        currentView.view.renderError(err.message);
-        return;
-      }
-
-      PAGE['error'].view.render();
+    if (currentView) currentView.view.render(data);
+  } catch (err: Error | unknown) {
+    if (currentView && err instanceof Error) {
+      currentView.view.renderError(err.message);
+      return;
     }
-};
+
+    PAGE['error'].view.render();
+  }
+}; 
 
 export const router = () => {
   window.addEventListener('DOMContentLoaded', route);
