d78ee0d45312f40210b986034e5590281ad03256
circlegiven <circlegiven@gmail.com>
2022-04-29 14:29:32 +0900

    [상품관리] 로컬스토리지에서 상품목록을 불러올때 생성자를 이용하여 상품을 생성하도록 변경 및 상품구매 로직 추가

--- src/js/ProductManage.js
+++ src/js/ProductManage.js
@@ -3,24 +3,27 @@ import Product from './Product.js';
 const LOCALSTORAGE_PRODUCT_MANAGE_KEY = 'circlegivenProductManage';
 
 const getProductListFromLocalStorage = () =>
-  JSON.parse(localStorage.getItem(LOCALSTORAGE_PRODUCT_MANAGE_KEY));
+  (JSON.parse(localStorage.getItem(LOCALSTORAGE_PRODUCT_MANAGE_KEY)) ?? []).map(
+    (data) => new Product({ ...data, enableEmptyQuantity: true })
+  );
 
 const updateProductListFromLocalStorage = (productList) => {
   localStorage.setItem(
     LOCALSTORAGE_PRODUCT_MANAGE_KEY,
-    JSON.stringify(
-      productList.map((product) =>
-        product instanceof Product ? product.toJson() : product
-      )
-    )
+    JSON.stringify(productList.map((product) => product.toJson()))
   );
 };
 
 const ProductManage = (() => {
   let productList = getProductListFromLocalStorage() ?? [];
 
-  const isDuplicateProduct = (product) =>
+  const isExistProduct = (product) =>
     productList.some((addedProduct) => product.name === addedProduct.name);
+
+  const isEmptyQuantityProduct = (product) => product.quantity <= 0;
+
+  const findProduct = (productName) =>
+    productList.find((product) => productName === product.name) ?? {};
 
   const updateProduct = (product) => {
     productList = productList.map((addedProduct) =>
@@ -38,13 +41,31 @@ const ProductManage = (() => {
     }
 
     // eslint-disable-next-line no-unused-expressions
-    isDuplicateProduct(product) ? updateProduct(product) : addProduct(product);
+    isExistProduct(product) ? updateProduct(product) : addProduct(product);
     updateProductListFromLocalStorage(productList);
+  };
+
+  const handlePurchaseProduct = (productName) => {
+    if (isExistProduct(findProduct(productName)) === false) {
+      throw new Error('상품이 존재하지 않습니다.');
+    }
+
+    if (isEmptyQuantityProduct(findProduct(productName))) {
+      throw new Error('상품의 수량이 남아있지않습니다.');
+    }
+
+    const changedProduct = new Product({
+      ...findProduct(productName).toJson(),
+      quantity: findProduct(productName).quantity - 1,
+      enableEmptyQuantity: true,
+    });
+    updateProduct(changedProduct);
   };
 
   return {
     list: () => productList,
     addProduct: handleAddProduct,
+    purchaseProduct: handlePurchaseProduct,
   };
 })();
 export default ProductManage;
