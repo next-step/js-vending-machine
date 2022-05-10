f8802af5dc401900aeec7249b7fcf1f834cf9ae9
dahye1013 <alrean@naver.com>
2022-05-10 17:42:48 +0900

    refactor: 도메인 상태 validator 모듈 분리

--- src/ts/state/store.ts
+++ src/ts/state/store.ts
@@ -1,7 +1,6 @@
+import { validateNewProduct, validateCoin } from './validator';
 import { getItem, setItem } from '../utils/storage';
-import { PRODUCT_CONFIG, COIN_CONFIG } from '../utils/config';
 import { ERROR } from '../utils/message';
-import { ValidationError } from '../utils/errorValidation';
 import { generateRandomNumber } from '../utils/randomGenerator';
 import { PathType } from '../router/pages';
 
@@ -33,7 +32,7 @@ export const loadData = <Obj extends StateTypes>(path: PathType): Obj => {
     state[pageName] = getItem(pageName);
     return state[pageName];
   } catch (err: unknown) {
-    throw err;
+    return [];
   }
 };
 
@@ -41,18 +40,6 @@ const sortProduct = () => {
   state.products = Array.from(<Array<Product>>state.products).sort((a: Product, b: Product) => {
     return a.name.localeCompare(b.name) || a.price - b.price || b.quantity - a.quantity;
   });
-};
-
-const validateNewProduct = (newProduct: Product) => {
-  if (newProduct.price < PRODUCT_CONFIG.MIN_PRICE) {
-    throw new ValidationError(ERROR.PRODUCT_LESS_THAN_MIN_PRICE);
-  }
-  if (newProduct.quantity < PRODUCT_CONFIG.MIN_QUANTITY) {
-    throw new ValidationError(ERROR.PRODUCT_LESS_THAN_MIN_QUANTITY);
-  }
-  if (newProduct.price % PRODUCT_CONFIG.SHOULD_BE_DIVIDED !== 0) {
-    throw new ValidationError(ERROR.PRODUCT_NOT_DIVIDED_PRICE);
-  }
 };
 
 export const addProduct = (newProduct: Product): Array<Product> => {
@@ -70,16 +57,6 @@ export const addProduct = (newProduct: Product): Array<Product> => {
     return state.products;
   } catch (err) {
     throw err;
-  }
-};
-
-const validateCoin = (inputPrice: number) => {
-  if (inputPrice < COIN_CONFIG.MIN_PRICE) {
-    throw new ValidationError(ERROR.COIN_LESS_THAN_MIN_PRICE);
-  }
-
-  if (inputPrice % COIN_CONFIG.SHOULD_BE_DIVIDED !== 0) {
-    throw new ValidationError(ERROR.COIN_NOT_DIVIDED_PRICE);
   }
 };
 
