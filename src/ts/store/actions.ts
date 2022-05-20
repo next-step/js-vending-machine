bd67e4e8bde084560557fab474f35c9feb5c5c8b
dahye1013 <alrean@naver.com>
2022-05-20 11:20:17 +0900

    fix: validation error 핸들링하는 try catch 구문 추가

--- src/ts/store/actions.ts
+++ src/ts/store/actions.ts
@@ -6,6 +6,7 @@ import {
   isValidPriceForBuyingProduct,
 } from '../state/validator';
 import { generateRandomNumber } from '../utils/randomGenerator';
+import { UserInputValidationError, InvalidStatusValidationError } from '../utils/errorValidation';
 
 export default {
   loadInitialData({ commit }) {
@@ -17,48 +18,73 @@ export default {
   },
 
   addProduct({ state, commit }, newProduct: Product) {
-    isValidForAddProduct(newProduct);
-    commit('addProduct', newProduct);
-    commit('sortProduct');
-    return state.products;
+    try {
+      isValidForAddProduct(newProduct);
+      commit('addProduct', newProduct);
+      commit('sortProduct');
+      return state.products;
+    } catch (err) {
+      if (err instanceof UserInputValidationError) {
+        alert(err.message);
+      }
+    }
   },
 
   chargeCoin({ state, commit }, inputPrice: number) {
-    isValidPriceForMakingCoin(inputPrice);
+    try {
+      isValidPriceForMakingCoin(inputPrice);
 
-    while (inputPrice > 0) {
-      const coinKeyRange = Reflect.ownKeys(state.coins).length - 1;
-      const randomKeyNumber = generateRandomNumber(0, coinKeyRange);
-      const selectedCoinKey = <CoinKey>Object.keys(state.coins)[randomKeyNumber];
-      const selectedCoin = <CoinObj>state.coins[selectedCoinKey];
+      while (inputPrice > 0) {
+        const coinKeyRange = Reflect.ownKeys(state.coins).length - 1;
+        const randomKeyNumber = generateRandomNumber(0, coinKeyRange);
+        const selectedCoinKey = <CoinKey>Object.keys(state.coins)[randomKeyNumber];
+        const selectedCoin = <CoinObj>state.coins[selectedCoinKey];
 
-      if (inputPrice >= selectedCoin.value) {
-        inputPrice -= selectedCoin.value;
-        commit('addCoin', selectedCoinKey);
+        if (inputPrice >= selectedCoin.value) {
+          inputPrice -= selectedCoin.value;
+          commit('addCoin', selectedCoinKey);
+        }
+      }
+
+      commit('saveCoins');
+
+      return state.coins;
+    } catch (err) {
+      if (err instanceof UserInputValidationError) {
+        alert(err.message);
       }
     }
-
-    commit('saveCoins');
-
-    return state.coins;
   },
 
   increaseInputPrice({ commit }, inputPrice: number) {
-    isValidInputPrice(inputPrice);
-    commit('increaseInputPrice', inputPrice);
-    commit('saveInputPrice');
+    try {
+      isValidInputPrice(inputPrice);
+
+      commit('increaseInputPrice', inputPrice);
+      commit('saveInputPrice');
+    } catch (err) {
+      if (err instanceof InvalidStatusValidationError) {
+        alert(err.message);
+      }
+    }
   },
 
   buyProduct({ state, commit }, productName: string) {
-    const product = state.products.find((product: Product) => product.name === productName);
-    const price = state.inputPrice;
+    try {
+      const product = state.products.find((product: Product) => product.name === productName);
+      const price = state.inputPrice;
 
-    isValidProductQuantity(product);
-    isValidPriceForBuyingProduct(product, price);
+      isValidProductQuantity(product);
+      isValidPriceForBuyingProduct(product, price);
 
-    commit('decreaseProductQuantity', product);
-    commit('decreaseInputPrice', product.price);
-    commit('saveInputPrice');
-    commit('saveProducts');
+      commit('decreaseProductQuantity', product);
+      commit('decreaseInputPrice', product.price);
+      commit('saveInputPrice');
+      commit('saveProducts');
+    } catch (err) {
+      if (err instanceof InvalidStatusValidationError) {
+        alert(err.message);
+      }
+    }
   },
 };
