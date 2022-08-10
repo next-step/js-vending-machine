50f9cc790b5da04c67b7664b740c78f078032011
ag502 <ag502@naver.com>
2022-08-11 00:22:24 +0900

    add: 상품 추가 탭 렌더링 로직 추가

---
+++ js/View/Tabs/ProductManageView.js
@@ -0,0 +1,48 @@
+import AbstractObserver from "../../util/observer/AbstractObserver.js";
+
+import { $ } from "../../util/dom.js";
+
+class ProductManageView extends AbstractObserver {
+	constructor(target) {
+		super();
+		this.$target = target;
+	}
+
+	static getTemplate() {
+		return /* html */ `
+            <div class="product-manage-tab">
+                <div class="product-container">
+                    <h3>상품 추가하기</h3>
+                    <input type="text" id="product-name-input" placeholder="상품명" />
+                    <input type="number" id="product-price-input" placeholder="가격" />
+                    <input type="number" id="product-quantity-input" placeholder="수량" />
+                    <button id="product-add-button">추가하기</button>
+                </div>
+                <table class="product-inventory">
+                    <colgroup>
+                        <col style="width: 140px" />
+                        <col style="width: 100px" />
+                        <col style="width: 100px" />
+                    </colgroup>
+                    <thead>
+                        <tr>
+                            <th>상품명</th>
+                            <th>가격</th>
+                            <th>수량</th>
+                        </tr>
+                    </thead>
+                    <tbody id="product-inventory-container"></tbody>
+                </table>
+            </div>
+        `;
+	}
+
+	render() {
+		const $producrManageTab = $(".product-manage-tab");
+
+		if ($producrManageTab) this.$target.removeChild($producrManageTab);
+		this.$target.insertAdjacentHTML("beforeend", ProductManageView.getTemplate());
+	}
+}
+
+export default ProductManageView;
