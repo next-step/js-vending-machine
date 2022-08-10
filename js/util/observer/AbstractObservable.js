67fe72a9c8c2da2883161fabbe265fbcb9473f58
ag502 <ag502@naver.com>
2022-08-10 23:26:54 +0900

    add: observable/observer abstract class 생성

---
+++ js/util/observer/AbstractObservable.js
@@ -0,0 +1,10 @@
+class AbstractObservable {
+	registerObserver() {}
+
+	removeObserver() {}
+
+	// eslint-disable-next-line no-unused-vars
+	notify(key, ...args) {}
+}
+
+export default AbstractObservable;
