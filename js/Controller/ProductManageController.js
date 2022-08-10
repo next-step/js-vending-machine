922288bf230cc37536553af1639d637dd66a5c9e
ag502 <ag502@naver.com>
2022-08-11 01:25:52 +0900

    add: ProductManageController 추가

---
+++ js/Controller/ProductManageController.js
@@ -0,0 +1,29 @@
+import AbstractObservable from "../util/observer/AbstractObservable.js";
+
+import ProductManageModel from "../Model/ProductManageModel.js";
+
+import { NOTIFY_KEY } from "../util/constants.js";
+
+class ProductManageController extends AbstractObservable {
+	constructor() {
+		super();
+		this.observers = [];
+
+		this.productManageModel = ProductManageModel;
+	}
+
+	registerObserver(view) {
+		this.observers.push(view);
+	}
+
+	notify(key, ...args) {
+		this.observers.forEach((observer) => observer.update(key, ...args));
+	}
+
+	handleAddProduct(productInfo) {
+		this.productManageModel.addProdcut(productInfo);
+		this.notify(NOTIFY_KEY.ADD_PRODUCT, productInfo);
+	}
+}
+
+export default new ProductManageController();
