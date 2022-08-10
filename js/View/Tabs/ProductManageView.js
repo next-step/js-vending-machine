49eefae129ec1d599a0e6796b6b19e8793332547
ag502 <ag502@naver.com>
2022-08-11 01:27:02 +0900

    add: 상품 추가 이벤트 로직 추가

--- js/View/Tabs/ProductManageView.js
+++ js/View/Tabs/ProductManageView.js
@@ -1,11 +1,15 @@
 import AbstractObserver from "../../util/observer/AbstractObserver.js";
 
+import ProductManageController from "../../Controller/ProductManageController.js";
+
 import { $ } from "../../util/dom.js";
 
 class ProductManageView extends AbstractObserver {
 	constructor(target) {
 		super();
 		this.$target = target;
+
+		this.initObserver();
 	}
 
 	static getTemplate() {
@@ -31,17 +35,68 @@ class ProductManageView extends AbstractObserver {
                             <th>수량</th>
                         </tr>
                     </thead>
-                    <tbody id="product-inventory-container"></tbody>
+                    <tbody id="product-inventory-container">
+                    </tbody>
                 </table>
             </div>
         `;
 	}
 
+	initObserver() {
+		this.productManageController = ProductManageController;
+		this.productManageController.registerObserver(this);
+	}
+
+	bindDom() {
+		this.$productNameInput = $("#product-name-input");
+		this.$productPriceInput = $("#product-price-input");
+		this.$productQuantityInput = $("#product-quantity-input");
+		this.$productAddBtn = $("#product-add-button");
+		this.$productTableBody = $("#product-inventory-container");
+	}
+
+	bindEvent() {
+		this.$productAddBtn.addEventListener("click", this.onAddProduct);
+	}
+
+	isProductInfoValid({ productName, productPrice, productQuantity }) {
+		return productName && !isNaN(productPrice) && !isNaN(productQuantity);
+	}
+
+	onAddProduct = () => {
+		const productName = this.$productNameInput.value;
+		const productPrice = this.$productPriceInput.valueAsNumber;
+		const productQuantity = this.$productQuantityInput.valueAsNumber;
+
+		const productInfo = {
+			productName,
+			productPrice,
+			productQuantity,
+		};
+
+		console.log(productInfo);
+
+		if (!this.isProductInfoValid(productInfo)) {
+			alert("모든 값은 필수 입니다");
+			return;
+		}
+
+		try {
+			this.productManageController.handleAddProduct(productInfo);
+		} catch (error) {
+			alert(error);
+		}
+	};
+
 	render() {
 		const $producrManageTab = $(".product-manage-tab");
 
 		if ($producrManageTab) this.$target.removeChild($producrManageTab);
+
 		this.$target.insertAdjacentHTML("beforeend", ProductManageView.getTemplate());
+
+		this.bindDom();
+		this.bindEvent();
 	}
 }
 
