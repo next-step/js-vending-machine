4fab71b24687e854e312e0370c096f961aec93b8
mingdy <alswl1500@naver.com>
2022-08-15 16:46:21 +0900

    feat: 상품 가격은 10원 단위로 나누어 떨어져야한다 테스트 코드 작성

--- cypress/e2e/vending-machine.cy.js
+++ cypress/e2e/vending-machine.cy.js
@@ -4,17 +4,34 @@ describe("lotto", () => {
   });
 
   it("자판기 상품을 추가하면 상품 현황에 나타난다.", () => {
-    cy.get("#product-name-input").type("oragne");
+    cy.get("#product-name-input").type("orange");
     cy.get("#product-price-input").type(300);
     cy.get("#product-quantity-input").type(6);
 
     cy.get("#product-add-button").click();
     cy.get("#product-inventory-container tr")
       .last()
-      .contains("oragne")
+      .contains("orange")
       .siblings()
       .contains(300)
       .siblings()
       .contains(6);
+  });
+
+  it("상품 가격은 10으로 나누어 떨어져야한다.", () => {
+    const alertStub = cy.stub();
+    cy.on("window:alert", alertStub);
+
+    cy.get("#product-name-input").type("apple");
+    cy.get("#product-price-input").type(333);
+    cy.get("#product-quantity-input").type(6);
+
+    cy.get("#product-add-button")
+      .click()
+      .then(() => {
+        expect(alertStub).to.be.calledWith(
+          "상품의 가격은 10원으로 나누어 떨어져야합니다."
+        );
+      });
   });
 });
