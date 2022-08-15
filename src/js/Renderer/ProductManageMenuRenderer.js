4ae8f3fde6935156cf4b5c84c78bdacf718838a1
mingdy <alswl1500@naver.com>
2022-08-15 20:46:48 +0900

    refactor: navigation 관리 부분과 상품관리 부분 분리

---
+++ src/js/Renderer/ProductManageMenuRenderer.js
@@ -0,0 +1,94 @@
+import store from "../store/index.js";
+class ProductManageMenuRenderer {
+  static MIN_PRODUCT_PRICE = 100;
+  static MIN_PRODUCT_COUNT = 1;
+  #app;
+
+  constructor($app) {
+    this.#app = $app;
+    this.initRenderer();
+    this.initEventListener();
+  }
+
+  initRenderer() {
+    this.#app.innerHTML = this.productManagerMenuTemplate();
+    if (!store.getTabState()[store.getCurrentTab()]) return;
+    const template = Object.keys(store.getTabState()[store.getCurrentTab()])
+      .map((key) => {
+        return this.productInventoryTemplate(
+          key,
+          store.getTabState()[store.getCurrentTab()][key]
+        );
+      })
+      .join("");
+    document
+      .querySelector("#product-inventory-container")
+      .insertAdjacentHTML("beforeend", template);
+  }
+
+  testProductPrice(price) {
+    return price % 10 == 0;
+  }
+
+  addProduct = (event) => {
+    //product 도메인 로직
+    event.preventDefault();
+    console.log(":왔니?");
+    if (!this.testProductPrice(event.target.children[1].value)) {
+      alert("상품의 가격은 10원으로 나누어 떨어져야합니다.");
+      return;
+    }
+    const state = store.getTabState();
+    state[store.getCurrentTab()][event.target.children[0].value] = {
+      price: event.target.children[1].value,
+      count: event.target.children[2].value,
+    };
+    store.setTabState(state);
+    this.initRenderer();
+  };
+
+  productInventoryTemplate(name, value) {
+    return /* html */ `
+    <tr>
+      <td>${name}</td>
+      <td>${value.price}</td>
+      <td>${value.count}</td>
+    </tr>
+    `;
+  }
+
+  productManagerMenuTemplate() {
+    return /* html */ `
+    <h3>상품 추가하기</h3>
+    <form class="product-container" id="product-container-form">
+      <input type="text" id="product-name-input" placeholder="상품명" autofocus required/>
+      <input type="number" id="product-price-input" placeholder="가격" min=${ProductManageMenuRenderer.MIN_PRODUCT_PRICE} required/>
+      <input type="number" id="product-quantity-input" placeholder="수량" min=${ProductManageMenuRenderer.MIN_PRODUCT_COUNT} required/>
+      <button type="submit" id="product-add-button" form="product-container-form">추가하기</button>
+    </form>
+    <table class="product-inventory">
+    <h3>상품 현황</h3>
+      <colgroup>
+        <col style="width: 140px" />
+        <col style="width: 100px" />
+        <col style="width: 100px" /> 
+      </colgroup>
+      <thead>
+        <tr>
+          <th>상품명</th>
+          <th>가격</th>
+          <th>수량</th>
+        </tr>
+      </thead>
+      <tbody id="product-inventory-container"></tbody>
+    </table>
+    `;
+  }
+  initEventListener() {
+    console.log("맞나");
+    document
+      .querySelector("#product-container-form")
+      .addEventListener("submit", this.addProduct);
+  }
+}
+export default ProductManageMenuRenderer;
--- src/js/store/index.js
+++ src/js/store/index.js
@@ -3,14 +3,20 @@ const store = {
     localStorage.setItem("tab", JSON.stringify(tab));
   },
   getCurrentTab() {
-    return JSON.parse(localStorage.getItem("tab"));
+    return JSON.parse(localStorage.getItem("tab")) || "product-manage-menu";
   },
 
   setTabState(state) {
     localStorage.setItem("state", JSON.stringify(state));
   },
   getTabState() {
-    return JSON.parse(localStorage.getItem("state"));
+    return (
+      JSON.parse(localStorage.getItem("state")) || {
+        "product-manage-menu": {},
+        "vending-machine-manage-menu": {},
+        "product-purchase-menu": {},
+      }
+    );
   },
 };
 
