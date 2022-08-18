dbfc865a1a77012e384692f2410315e80aaaf519
jlee0505 <hj960322@gmail.com>
2022-08-19 08:03:36 +0900

    feat: 상품관리 기능

---
+++ js/View/MainRenderer.js
@@ -0,0 +1,47 @@
+import store from "../util/store/store.js";
+import { ProductManageMenuRenderer } from "./ProductManageMenuRenderer.js";
+import { VendingMachineMenuRenderer } from "./VendingMachineMenuRenderer.js";
+
+import { MENU_TAB } from "../util/constants.js";
+
+export class MainRenderer {
+  constructor($app) {
+    this.app = $app;
+
+    this.initStorage();
+    this.initRenderer();
+    this.initEventListener();
+  }
+
+  initStorage() {
+    if (!store.getCurrentTab()) store.setCurrentTab(MENU_TAB.PRODUCT_MANAGE);
+    if (!store.getTabState())
+      store.setTabState({
+        "product-manage-menu": {},
+        "vending-machine-manage-menu": {},
+        "product-purchase-menu": {},
+      });
+  }
+
+  initRenderer() {
+    switch (store.getCurrentTab()) {
+      case MENU_TAB.PRODUCT_MANAGE:
+        return new ProductManageMenuRenderer(this.app);
+      case MENU_TAB.VM_MANAGE:
+        return new VendingMachineMenuRenderer(this.app);
+      case MENU_TAB.PRODUCT_PURCHASE:
+        return;
+    }
+  }
+
+  initEventListener() {
+    document.querySelector("nav").addEventListener("click", (e) => {
+      const isNavBtn = e.target.classList.contains("vending-machine-menu");
+      if (isNavBtn) {
+        store.setCurrentTab(e.target.id);
+        console.log(e.target.id);
+        this.initRenderer();
+      }
+    });
+  }
+}
