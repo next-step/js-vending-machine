5ba180f02d7b3fb07e877122ee5356bdf20dbc89
wondonghwi <ehdehd1289@naver.com>
2022-09-30 01:52:30 +0900

    ✨ 각 탭의 내용(state)를 LS에 저장하기 위한 Storage 로직 작성

--- src/js/storage/index.js
+++ src/js/storage/index.js
@@ -6,6 +6,20 @@ class Storage {
   static getCurrentTab() {
     return localStorage.getItem('tab');
   }
+
+  static setStateData(state) {
+    localStorage.setItem('state', JSON.stringify(state));
+  }
+
+  static getStateData() {
+    return (
+      JSON.parse(localStorage.getItem('state')) || {
+        'product-manage-menu': {},
+        'vending-machine-manage-menu': {},
+        'product-purchase-menu': {},
+      }
+    );
+  }
 }
 
 export default Storage;
