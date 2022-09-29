08a205c21551383d209517b08dc8eba1d1561795
wondonghwi <ehdehd1289@naver.com>
2022-09-30 01:44:21 +0900

    ✨ 상품 추가 템플릿 작성

--- src/js/view/ProductManageMenu.js
+++ src/js/view/ProductManageMenu.js
@@ -4,10 +4,34 @@ class ProductManageMenu {
     this.initRenderer();
   }
 
-  initRenderer() {
-    this.app.innerHTML = `
-    <h3>상품 추가 하가</h3>
+  productManagerMenuTemplate = `
+    <h3>상품 추가하기</h3>
+    <form class="product-container" id="product-container-form">
+      <input type="text" id="product-name-input" placeholder="상품명" autofocus required/>
+      <input type="number" id="product-price-input" placeholder="가격" min=${MIN_PRODUCT.PRICE} required/>
+      <input type="number" id="product-quantity-input" placeholder="수량" min=${MIN_PRODUCT.COUNT} required/>
+      <button type="submit" id="product-add-button">추가하기</button>
+    </form>
+    <table class="product-inventory">
+    <h3>상품 현황</h3>
+      <colgroup>
+        <col style="width: 140px" />
+        <col style="width: 100px" />
+        <col style="width: 100px" /> 
+      </colgroup>
+      <thead>
+        <tr>
+          <th>상품명</th>
+          <th>가격</th>
+          <th>수량</th>
+        </tr>
+      </thead>
+      <tbody id="product-inventory-container"></tbody>
+    </table>
     `;
+
+  initRenderer() {
+    this.app.innerHTML = this.productManagerMenuTemplate;
   }
 }
 export default ProductManageMenu;
