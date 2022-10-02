1406c10812d49f9ca61698a74a6e8f570cb31510
wondonghwi <ehdehd1289@naver.com>
2022-10-03 02:29:38 +0900

    ✨ 0 ~ 매개변수로 받은 숫자만큼 랜덤한값을 return 해주는 랜덤함수 작성

--- src/js/utils/index.js
+++ src/js/utils/index.js
@@ -1,3 +1,5 @@
 const removeSpaces = string => string.replace(/\s/g, '');
 
-export { removeSpaces };
+const coinRandomRange = range => Math.floor(Math.random() * (range + 1));
+
+export { removeSpaces, coinRandomRange };
