7f1d7d9c2d39758ef8cf935c00af707a15486c11
wondonghwi <ehdehd1289@naver.com>
2022-10-22 21:48:30 +0900

    💄 현재선택된 탭이 어떤탭인지 알 수 있도록 선택된탭 스타일 작업

--- src/js/index.js
+++ src/js/index.js
@@ -6,16 +6,28 @@ import { MENU } from './constants/index.js';
 
 const $app = document.querySelector('#app');
 const $navigation = document.querySelector('.navigation');
+const buttonMenu = document.querySelectorAll('.button-menu');
 
 const renderTab = () => {
+  buttonMenu.forEach(element => {
+    element.classList.remove('clicked');
+  });
+
   switch (LocalStorage.getCurrentTab()) {
     case MENU.PRODUCT_MANAGE:
+      buttonMenu[0].classList.add('clicked');
       return new ProductManageMenu($app);
+
     case MENU.VENDING_MACHINE_MANAGE:
+      buttonMenu[1].classList.add('clicked');
       return new VendingMachineManageMenu($app);
+
     case MENU.PRODUCT_PURCHASE:
+      buttonMenu[2].classList.add('clicked');
       return new ProductPurchaseMenu($app);
+
     default:
+      buttonMenu[0].classList.add('clicked');
       return new ProductManageMenu($app);
   }
 };
