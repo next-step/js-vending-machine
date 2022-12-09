04f560637eda61b09a0450b86cd8329b293d61fd
Seung-wan <tmddhks0104@gmail.com>
2022-12-10 03:09:14 +0900

    Feat: 비워진 상태인 최초 상품 목록을 확인하는 테스트코드 작성

--- cypress/e2e/vendingMachine.cy.js
+++ cypress/e2e/vendingMachine.cy.js
@@ -1,5 +1,11 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io');
+beforeEach(() => {
+  cy.visit('index.html');
+});
+
+describe('상품관리 탭을 테스트한다.', () => {
+  context('상품을 추가할 때', () => {
+    it('최초 상품 목록은 비워진 상태이다.', () => {
+      cy.get('#product-inventory-container').children().should('have.length', 0);
+    });
   });
 });
