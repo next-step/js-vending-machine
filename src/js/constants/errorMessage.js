699d225f46ec56bb69ac518baaa1499d715b0b24
Seung-wan <tmddhks0104@gmail.com>
2022-12-16 04:25:20 +0900

    Fix: 에러메시지 상수 사용

--- src/js/constants/errorMessage.js
+++ src/js/constants/errorMessage.js
@@ -1,10 +1,12 @@
+import { CHARGE, PRODUCT } from './vendingMachine.js';
+
 export const ERROR_MESSAGE = {
   EMPTY_INPUT: '공백은 추가할 수 없습니다.',
   INVALID_PRODUCT_NAME: '유효하지 않은 상품명입니다.',
-  INVALID_PRODUCT_MIN_PRICE: '상품의 최소 가격은 100원입니다.',
-  INVALID_PRODUCT_PRICE_UNIT: '상품의 가격은 10원으로 나누어 떨어져야 합니다.',
-  INVALID_PRODUCT_MIN_QUANTITY: '상품의 최소 수량은 1개여야 합니다.',
+  INVALID_PRODUCT_MIN_PRICE: `상품의 최소 가격은 ${PRODUCT.MIN_PRICE}원입니다.`,
+  INVALID_PRODUCT_PRICE_UNIT: `상품의 가격은 ${PRODUCT.DIVISIBLE_UNIT}원으로 나누어 떨어져야 합니다.`,
+  INVALID_PRODUCT_MIN_QUANTITY: `상품의 최소 수량은 ${PRODUCT.MIN_QUANTITY}개여야 합니다.`,
 
-  INVALID_VENDING_MACHINE_MIN_CHARGE: '최소 충전 금액은 100원 이상만 가능합니다.',
-  INVALID_VENDING_MACHINE_CHARGE_UNIT: '10원으로 나누어 떨어지는 금액만 충전이 가능합니다.',
+  INVALID_VENDING_MACHINE_MIN_CHARGE: `최소 충전 금액은 ${CHARGE.MIN_PRICE}원 이상만 가능합니다.`,
+  INVALID_VENDING_MACHINE_CHARGE_UNIT: `${CHARGE.DIVISIBLE_UNIT}원으로 나누어 떨어지는 금액만 충전이 가능합니다.`,
 };
