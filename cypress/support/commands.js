e41a1b1a7485f4cae83454ed76cd80aa82b97e61
Seung-wan <tmddhks0104@gmail.com>
2022-12-16 05:50:05 +0900

    Refactor: 테스트코드 addAll 분리

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,55 +1,56 @@
 import { SELECTOR } from '../../src/js/constants/selector.js';
 
-Cypress.Commands.add('typeProductName', (name) => {
-  cy.get(SELECTOR.PRODUCT_NAME_INPUT).type(name);
+Cypress.Commands.addAll({
+  typeProductName(name) {
+    cy.get(SELECTOR.PRODUCT_NAME_INPUT).type(name);
+  },
+  typeProductPrice(price) {
+    cy.get(SELECTOR.PRODUCT_PRICE_INPUT).type(price);
+  },
+  typeProductQuantity(quantity) {
+    cy.get(SELECTOR.PRODUCT_QUANTITY_INPUT).type(quantity);
+  },
+  clickProductAddButton() {
+    cy.get(SELECTOR.PRODUCT_ADD_BUTTON).click();
+  },
+  addProduct({ name, price, quantity }) {
+    cy.typeProductName(name);
+    cy.typeProductPrice(price);
+    cy.typeProductQuantity(quantity);
+    cy.clickProductAddButton();
+  },
 });
 
-Cypress.Commands.add('typeProductPrice', (price) => {
-  cy.get(SELECTOR.PRODUCT_PRICE_INPUT).type(price);
+Cypress.Commands.addAll({
+  typeCharge(charge) {
+    cy.get(SELECTOR.VENDING_MACHINE_CHARGE_INPUT).type(charge);
+  },
+
+  clickAddChargeButton() {
+    cy.get(SELECTOR.VENDING_MACHINE_CHARGE_BUTTON).click();
+  },
+  addCharge(charge) {
+    cy.typeCharge(charge);
+    cy.clickAddChargeButton();
+  },
 });
 
-Cypress.Commands.add('typeProductQuantity', (quantity) => {
-  cy.get(SELECTOR.PRODUCT_QUANTITY_INPUT).type(quantity);
-});
+Cypress.Commands.addAll({
+  checkCoinFormat(selector) {
+    cy.get(selector)
+      .invoke('text')
+      .then((text) => {
+        const textArray = text.split('');
+        const textArrayLength = textArray.length;
 
-Cypress.Commands.add('clickProductAddButton', () => {
-  cy.get(SELECTOR.PRODUCT_ADD_BUTTON).click();
-});
-
-Cypress.Commands.add('addProduct', ({ name, price, quantity }) => {
-  cy.typeProductName(name);
-  cy.typeProductPrice(price);
-  cy.typeProductQuantity(quantity);
-  cy.clickProductAddButton();
-});
-
-Cypress.Commands.add('typeCharge', (charge) => {
-  cy.get(SELECTOR.VENDING_MACHINE_CHARGE_INPUT).type(charge);
-});
-
-Cypress.Commands.add('clickAddChargeButton', () => {
-  cy.get(SELECTOR.VENDING_MACHINE_CHARGE_BUTTON).click();
-});
-
-Cypress.Commands.add('addCharge', (charge) => {
-  cy.typeCharge(charge);
-  cy.clickAddChargeButton();
-});
-
-Cypress.Commands.add('checkCoinFormat', (selector) => {
-  cy.get(selector)
-    .invoke('text')
-    .then((text) => {
-      const textArray = text.split('');
-      const textArrayLength = textArray.length;
-
-      textArray.forEach((txt, idx) => {
-        if (idx === textArrayLength - 1) {
-          expect(txt).to.be.equal('개');
-          return;
-        }
-        // eslint-disable-next-line no-unused-expressions
-        expect(Number.isNaN(Number(txt))).to.be.false;
+        textArray.forEach((txt, idx) => {
+          if (idx === textArrayLength - 1) {
+            expect(txt).to.be.equal('개');
+            return;
+          }
+          // eslint-disable-next-line no-unused-expressions
+          expect(Number.isNaN(Number(txt))).to.be.false;
+        });
       });
-    });
+  },
 });
