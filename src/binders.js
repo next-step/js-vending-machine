67d841d96054bdce548d4cc02930f1bc5998dbf2
Woo KyungJun <natcho9010@gmail.com>
2022-12-22 00:30:57 +0900

    refactor: binder

--- src/binders.js
+++ src/binders.js
@@ -1,8 +1,46 @@
-export const binders ={
+import { entryObject } from './utils/utils.js';
+
+export const binders = {
   productContainerBinder: null,
   productInventoryContainerBinder: null,
 };
 
 export function render(binderName) {
   binders[binderName]?.();
+}
+
+export function createBinder(view, viewModelCreator) {
+  const render = () => {
+    const vm = viewModelCreator();
+      const el = bindViewModelWithView(vm, view);
+      view.render(el);
+  }
+  render();
+
+  return render;
+}
+
+function bindViewModelWithView(vm, view) {
+  entryObject(vm).forEach(([elementName, elementViewModel]) => {
+    const pairElement = view[elementName];
+    entryObject(elementViewModel).forEach(([key, val]) => {
+      if (key === 'events') {
+        entryObject(val).forEach(([eventType, callback]) => {
+          pairElement.addEventListener(eventType, callback);
+        });
+        return;
+      }
+
+      if (key === 'ref') {
+        val.element = pairElement;
+        return;
+      }
+
+      if (key === 'children') {
+        pairElement.innerHTML = val;
+      }
+    });
+  });
+
+  return view.rootElement;
 }
--- src/index.js
+++ src/index.js
@@ -5,8 +5,7 @@ import { getVendingMachineManageMenuHTML } from './components/vendingMachineMana
 import { productManagerController } from './controller/productManagerController.js';
 import { productInventoryContainerController } from './controller/productManagerController.js';
 
-import { binders } from './binders.js';
-import { entryObject } from './utils/utils.js';
+import { binders, createBinder } from './binders.js';
 
 const $app = document.getElementById('app');
 const $productManageMenu = document.getElementById('product-manage-menu');
@@ -22,43 +21,6 @@ $productManageMenu.addEventListener('click', () => {
   binders.productContainerBinder = createBinder(productContainerView, productManagerController);
   binders.productInventoryContainerBinder = createBinder(productInventoryContainerView, productInventoryContainerController);
 });
-
-function createBinder(view, viewModelCreator) {
-
-  function bindViewModelWithView(vm, view) {
-    entryObject(vm).forEach(([elementName, elementViewModel]) => {
-      const pairElement = view[elementName];
-      entryObject(elementViewModel).forEach(([key, val]) => {
-        if (key === 'events') {
-          entryObject(val).forEach(([eventType, callback]) => {
-            pairElement.addEventListener(eventType, callback);
-          });
-          return;
-        }
-
-        if (key === 'ref') {
-          val.element = pairElement;
-          return;
-        }
-
-        if (key === 'children') {
-          pairElement.innerHTML = val;
-        }
-      });
-    });
-
-    return view.rootElement;
-  }
-
-  const render = () => {
-    const vm = viewModelCreator();
-      const el = bindViewModelWithView(vm, view);
-      view.render(el);
-  }
-  render();
-
-  return render;
-}
 
 $vendingMachineManageMenu.addEventListener('click', () => {
   $app.innerHTML = getVendingMachineManageMenuHTML();
