72ff64a17b27a91a3169908d9b7e45ba44a54cb1
Woo KyungJun <natcho9010@gmail.com>
2022-12-22 00:26:07 +0900

    feat: implement binders in index

--- src/index.js
+++ src/index.js
@@ -2,6 +2,12 @@ import { getProductManagerHTML, viewInitiator } from './components/productManage
 import { getProductPurchaseHTML } from './components/productPurchase.js';
 import { getVendingMachineManageMenuHTML } from './components/vendingMachineManageMenu.js';
 
+import { productManagerController } from './controller/productManagerController.js';
+import { productInventoryContainerController } from './controller/productManagerController.js';
+
+import { binders } from './binders.js';
+import { entryObject } from './utils/utils.js';
+
 const $app = document.getElementById('app');
 const $productManageMenu = document.getElementById('product-manage-menu');
 const $vendingMachineManageMenu = document.getElementById('vending-machine-manage-menu');
@@ -12,7 +18,47 @@ $productManageMenu.addEventListener('click', () => {
     productContainerView,
     productInventoryContainerView,
   } = viewInitiator($app, getProductManagerHTML());
+
+  binders.productContainerBinder = createBinder(productContainerView, productManagerController);
+  binders.productInventoryContainerBinder = createBinder(productInventoryContainerView, productInventoryContainerController);
 });
+
+function createBinder(view, viewModelCreator) {
+
+  function bindViewModelWithView(vm, view) {
+    entryObject(vm).forEach(([elementName, elementViewModel]) => {
+      const pairElement = view[elementName];
+      entryObject(elementViewModel).forEach(([key, val]) => {
+        if (key === 'events') {
+          entryObject(val).forEach(([eventType, callback]) => {
+            pairElement.addEventListener(eventType, callback);
+          });
+          return;
+        }
+
+        if (key === 'ref') {
+          val.element = pairElement;
+          return;
+        }
+
+        if (key === 'children') {
+          pairElement.innerHTML = val;
+        }
+      });
+    });
+
+    return view.rootElement;
+  }
+
+  const render = () => {
+    const vm = viewModelCreator();
+      const el = bindViewModelWithView(vm, view);
+      view.render(el);
+  }
+  render();
+
+  return render;
+}
 
 $vendingMachineManageMenu.addEventListener('click', () => {
   $app.innerHTML = getVendingMachineManageMenuHTML();
