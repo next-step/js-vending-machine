506b313cc28f463a25b1aacde8cdf1fd8011ab32
Woo KyungJun <natcho9010@gmail.com>
2022-12-24 15:46:06 +0900

    feat: add state save and load in localStorage

--- src/components/vendingMachineManagerComponent.js
+++ src/components/vendingMachineManagerComponent.js
@@ -1,4 +1,5 @@
 import { render } from "../binders.js";
+import { getLocalStorageItem, setLocalStorageItem } from "../utils/localStorageUtils.js";
 import { divideNumberInCountOfDivideLevels } from "../utils/utils.js";
 
 import { Ref } from "./common/Ref.js";
@@ -7,7 +8,10 @@ const vendingMachineInitState = {
   amount: 0,
 };
 
-let vendingMachineState = vendingMachineInitState;
+let vendingMachineState = getLocalStorageItem('vending-machine-manager', (item) => {
+  if (!item) return;
+  return JSON.parse(item);
+}) || vendingMachineInitState;
 
 const chargeAmountInputRef = new Ref();
 
@@ -21,18 +25,18 @@ export function vendingMachineControllerComponent() {
         const inputAmount = chargeAmountInputRef.element.value;
         if (!inputAmount && inputAmount > 10) {
           alert('충전 금액을 10원 이상 입력해주세요!');
-          chargeAmountInput.element.focus();
+          chargeAmountInputRef.element.focus();
           return;
         }
 
-        // TODO: 10원 단위 확인하는 것 util로 빼기
         if (inputAmount % 10 !== 0) {
           alert('충전 금액을 10원 단위로 입력해주세요!');
-          chargeAmountInput.element.focus();
+          chargeAmountInputRef.element.focus();
           return;
         }
 
         vendingMachineState.amount = inputAmount;
+        setLocalStorageItem('vending-machine-manager', JSON.stringify(vendingMachineState));
         render('cashBoxBinder');
       }},
     },
@@ -46,7 +50,10 @@ const coin50Ref = new Ref();
 const coin10Ref = new Ref();
 
 export function cashBoxComponent() {
-  totalAmountRef.executeElementCallback((totalAmountElement) => totalAmountElement.textContent = vendingMachineState.amount)
+  totalAmountRef.addOnRenderCallback((totalAmountElement) => {
+    console.log(totalAmountElement, vendingMachineState)
+    totalAmountElement.textContent = vendingMachineState.amount
+  });
 
   const [
     coin500Count,
@@ -57,10 +64,10 @@ export function cashBoxComponent() {
     ? divideNumberInCountOfDivideLevels(vendingMachineState.amount, [500, 100, 50, 10])
     : [];
 
-  coin500Ref.executeElementCallback((coin500Element) => coin500Element.textContent = coin500Count);
-  coin100Ref.executeElementCallback((coin100Element) => coin100Element.textContent = coin100Count);
-  coin50Ref.executeElementCallback((coin50Element) => coin50Element.textContent = coin50Count);
-  coin10Ref.executeElementCallback((coin10Element) => coin10Element.textContent = coin10Count);
+  coin500Ref.addOnRenderCallback((coin500Element) => coin500Element.textContent = coin500Count);
+  coin100Ref.addOnRenderCallback((coin100Element) => coin100Element.textContent = coin100Count);
+  coin50Ref.addOnRenderCallback((coin50Element) => coin50Element.textContent = coin50Count);
+  coin10Ref.addOnRenderCallback((coin10Element) => coin10Element.textContent = coin10Count);
 
   return {
     totalAmount: {
