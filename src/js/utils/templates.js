0f2be8af518acf9594c5faf0e2712253644d6f1b
publee <publizm@gmail.com>
2023-01-05 00:01:56 +0900

    fix: html class attribute 오타 수정

--- src/js/utils/templates.js
+++ src/js/utils/templates.js
@@ -63,15 +63,15 @@ export const PRODUCT_CONTAINER_TEMPLATE = `<div>
   </div>`;
 
 export const COIN_CHARGING_TEMPLATE = `<div>
-  <section className="mb-10">
-    <h2 className="mb-5 text-center text-2xl font-bold">
+  <section class="mb-10">
+    <h2 class="mb-5 text-center text-2xl font-bold">
       자판기 동전 충전하기
     </h2>
-    <form className="flex justify-center">
-      <div className="flex shrink">
+    <form class="flex justify-center">
+      <div class="flex shrink">
         <input
           type="number"
-          className="product-input w-full"
+          class="product-input w-full"
           name="product-quantity"
           placeholder="자판기가 보유할 금액"
           min="0"
@@ -80,46 +80,46 @@ export const COIN_CHARGING_TEMPLATE = `<div>
       <button
         type="submit"
         id="product-add-button"
-        className="ml-3 shrink-0 border px-3 py-1"
+        class="ml-3 shrink-0 border px-3 py-1"
       >
         충전하기
       </button>
     </form>
     <p
       id="retention-amount"
-      className="mx-auto mt-8 box-border w-8/12 rounded-xl border border-2 border-dashed p-3 text-center"
+      class="mx-auto mt-8 box-border w-8/12 rounded-xl border border-2 border-dashed p-3 text-center"
     >
       보유 금액: 0원
     </p>
   </section>
   <section>
-    <h2 className="mb-5 text-2xl font-bold">자판기가 보유한 동전</h2>
+    <h2 class="mb-5 text-2xl font-bold">자판기가 보유한 동전</h2>
     <table
       id="product-inventory"
-      className="w-full overflow-hidden rounded-md border bg-slate-100"
+      class="w-full overflow-hidden rounded-md border bg-slate-100"
     >
       <thead>
-      <tr className="border-slate-100 bg-slate-600 text-white">
-        <th className="w-1/2 py-2 font-normal">동전</th>
-        <th className="w-1/2 border-l py-2 font-normal">개수</th>
+      <tr class="border-slate-100 bg-slate-600 text-white">
+        <th class="w-1/2 py-2 font-normal">동전</th>
+        <th class="w-1/2 border-l py-2 font-normal">개수</th>
       </tr>
       </thead>
       <tbody id="product-inventory-container">
-      <tr className="border-t">
-        <td className="py-1 text-center">500원</td>
-        <td className="border-l py-2 text-center">2</td>
+      <tr class="border-t">
+        <td class="py-1 text-center">500원</td>
+        <td class="border-l py-2 text-center">2</td>
       </tr>
-      <tr className="border-t">
-        <td className="py-2 text-center">100원</td>
-        <td className="border-l py-2 text-center">2</td>
+      <tr class="border-t">
+        <td class="py-2 text-center">100원</td>
+        <td class="border-l py-2 text-center">2</td>
       </tr>
-      <tr className="border-t">
-        <td className="py-2 text-center">50원</td>
-        <td className="border-l py-2 text-center">2</td>
+      <tr class="border-t">
+        <td class="py-2 text-center">50원</td>
+        <td class="border-l py-2 text-center">2</td>
       </tr>
-      <tr className="border-t">
-        <td className="py-2 text-center">10원</td>
-        <td className="border-l py-1 text-center">2</td>
+      <tr class="border-t">
+        <td class="py-2 text-center">10원</td>
+        <td class="border-l py-1 text-center">2</td>
       </tr>
       </tbody>
     </table>
