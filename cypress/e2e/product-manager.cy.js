d85f9b99e8c23196ae6de1baedd4ec5af96f2e1b
golani <go.lani.developer@gmail.com>
2023-01-06 14:44:26 +0900

    test: 메뉴 테스트 / 메뉴별 테스트 분리

--- cypress/e2e/product-manager.cy.js
+++ cypress/e2e/product-manager.cy.js
@@ -1,32 +1,9 @@
-const MENU_SELECTOR = "#menu";
-
 describe("상품관리 테스트", () => {
   beforeEach(() => {
     cy.visit("/");
   });
-  context("메뉴 탭 UI 테스트", () => {
-    it("메뉴 탭 리스트 UI가 화면에 존재한다.", () => {
-      cy.get(MENU_SELECTOR).should("exist");
-    });
-
-    it("메뉴는 상품 관리, 잔돈 충전, 상품 구매가 있다.", () => {
-      cy.get(`${MENU_SELECTOR} button`).should(($menuButton) => {
-        expect($menuButton).to.have.length(3);
-
-        const menuTexts = $menuButton.map((_, el) => el.innerText);
-
-        expect(menuTexts.get()).to.deep.equal([
-          "상품 관리",
-          "잔돈 충전",
-          "상품 구매",
-        ]);
-      });
-    });
-  });
 
   context("상품을 추가할 수 있다.", () => {
-    it("상품을 추가할 수 있는 화면이 존재한다.", () => {
-      cy.contains("상품 추가하기");
-    });
+    it("상품을 추가할 수 있는 화면이 존재한다.", () => {});
   });
 });
---
+++ cypress/support/selectors.js
@@ -0,0 +1,4 @@
+export const MENU_SELECTOR = "#menu";
+export const PRODUCT_MANAGE_MENU_SELECTOR = "#product-manage-menu";
+export const CHANGE_CHARGE_MENU_SELECTOR = "#change-charge-menu";
+export const PRODUCT_PURCHASE_MENU_SELECTOR = "#product-purchase-menu";
