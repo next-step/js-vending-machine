f9dc53c97df4d35d84ac4973cd9618a74db479fd
Woo KyungJun <natcho9010@gmail.com>
2023-01-07 13:59:08 +0900

    feat: add CustomComponent:

--- src/core/CustomComponent.js
+++ src/core/CustomComponent.js
@@ -1,40 +1,74 @@
-import { mergeArrayObjectToOneObject } from "../utils/utils";
+import { entryObject } from "../utils/utils.js";
 
-export class CustomElement extends HTMLElement {
-  elements = {};
+export function getCustomElementClass(Element) {
+  class CustomElement extends Element {
+    elements = {};
+    dynamicElementSelectors = {};
 
-  constructor(children = {}) {
-    super();
-    // children들의 이름들을 모두 elements안에 property로 등록한다.
-    // 매번 DOM을 탐색해서 가져오는 것은 비효율적이기 때문이다.
-    // 따라서 DOM interface를 해치지 않도록 elements라는 곳 안에 1차원 depth로 모두 넣어준다.
-    this.elements = this.#flatDOMLikeObj(children);
-    // children들은 모두 DOM을 지켜서 내려준다.
-    this.appendChild(children);
-  }
-
-  #flatDOMLikeObj(obj) {
-    const allElements = [];
-    const stack = [obj];
-    let currentElement = null;
-
-    while(stack.length > 0) {
-      currentElement = stack.pop();
-      // element를 등록한다.
-      allElements.push(currentElement);
-      // 자식이 있으면 그 아래로 내려간다.
-      if (currentElement.children) {
-        stack.push(currentElement.children);
-      }
+    constructor() {
+      super();
     }
 
-    // allElements에 모은 모든 element를 하나의 객체로
-    return mergeArrayObjectToOneObject(allElements);
+    connectedCallback() {
+      this.createElements();
+    }
+
+    disconnectedCallback() {
+      this.elements = {};
+    }
+
+    init(children = [], dynamicElementSelectors = {}) {
+      this.append(...children);
+      this.dynamicElementSelectors = dynamicElementSelectors;
+    }
+
+    createElements() {
+      this.scanDynamicElement();
+      this.elements.rootElement = this;
+    }
+
+    scanDynamicElement() {
+      entryObject(this.dynamicElementSelectors).forEach(([name, selector]) => {
+        const element = this.querySelector(selector);
+        if (!element) throw new Error(`selector "${selector}" does not exist`);
+        this.elements[name] = element;
+      });
+    }
+
+    attachEvent(eventObject) {
+      entryObject(eventObject).forEach(([name, [eventType, eventCallback]]) => {
+        this.elements[name].addEventListener(eventType, eventCallback);
+      });
+    }
+
+    render(newElement) {
+      if (newElement) {
+        this.replaceWith(newElement);
+        this.createElements();
+      }
+    }
   }
 
-  attachEvent(eventObject) {
-    entryObject(eventObject).forEach(([name, [eventType, eventCallback]]) => {
-      this.elements[name].addEventListener(eventType, eventCallback);
-    });
+  return CustomElement;
+}
+
+export function getCustomElementCreator({
+  elementType,
+  customElementName,
+}) {
+  const customElement = document.createElement(elementType, { is: customElementName });
+
+  return ({
+    children,
+    dynamicElementSelectors,
+    attributes,
+  }) => {
+    customElement.init(children, dynamicElementSelectors);
+    for (const key in attributes) {
+      const val = attributes[key];
+      customElement[key] = val;
+    }
+
+    return customElement;
   }
 }
