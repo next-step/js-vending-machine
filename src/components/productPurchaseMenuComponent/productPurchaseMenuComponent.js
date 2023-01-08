3a4f6f43aae632f47bd08d79500efccd3636242b
Woo KyungJun <natcho9010@gmail.com>
2023-01-08 23:43:45 +0900

    feat: sync & validate coin Input

--- src/components/productPurchaseMenuComponent/productPurchaseMenuComponent.js
+++ src/components/productPurchaseMenuComponent/productPurchaseMenuComponent.js
@@ -1,25 +1,41 @@
-import { render, COIN_INPUT_DISPLAY_BINDER } from '../../binders.js';
+import { render, COIN_INPUT_DISPLAY_BINDER, REST_AMOUNT_FLUSH_DISPLAY_BINDER } from '../../binders.js';
 import { setLocalStorageItem } from '../../utils/localStorageUtils.js';
 import { products, PRODUCTS_STATE_KEY } from '../../states/productState.js';
 
+import { ProductPurchaseMenuState } from './ProductPurchaseMenuState.js';
+
 import { Ref } from '../common/Ref.js';
 
-const coinInputControllerInitialState = {
-  amount: 0,
-};
+// 사용자가 넣은 금액은 전역으로 알 필요가 없다.
+// TODO: 객체로 묶기
+const coinInputControllerInitialState = new ProductPurchaseMenuState({
+  input: 0,
+  totalAmount: 0,
+});
+// 상품 금액만큼 차감되고 상품의 수량도 차감된다.
+// 수량이 0인상품은 구매할 수 없다.
+// 구매하려는 상품가격이 보유하고 있는 금액보다 높은 경우 상품을 구매할 수 없다.
+
+// 반환하기버튼을 통해 잔돈을 반환받을 수 있다.
+
+const coinInputRef = new Ref();
 
 export function coinInputControllerComponent() {
   let coinInputControllerState = coinInputControllerInitialState;
 
   return {
     coinInput: {
+      ref: coinInputRef,
       events: {change: (e) => {
-        coinInputControllerState.amount = e.target.value
+        coinInputControllerState.syncInput(e.target.value);
       }},
     },
     coinSubmitButton: {
-      events: {click: (e) => {
-        // TODO: 전역 상태로 올리기
+      events: {click: () => {
+        if (!coinInputControllerState.validateAmountInput(coinInputRef.element)) return;
+
+        coinInputRef.element.value = null;
+        coinInputControllerState.accumulateInput();
         render(COIN_INPUT_DISPLAY_BINDER);
       }},
     },
@@ -29,7 +45,7 @@ export function coinInputControllerComponent() {
 const insertAmountRef = new Ref();
 
 export function coinInputDisplayComponent() {
-  insertAmountRef.addOnRenderCallback((insertAmount) => insertAmount.textContent = coinInputControllerInitialState.amount);
+  insertAmountRef.addOnRenderCallback((insertAmount) => insertAmount.textContent = `${coinInputControllerInitialState.totalAmount}원`);
 
   return {
     insertAmount: {
@@ -39,16 +55,45 @@ export function coinInputDisplayComponent() {
 }
 
 export function productListComponent() {
-  return {}
+  // TODO: 구매하기 버튼에 products를 갱신하고 productList를 다시 그려주는 로직을 심어서 보내준다.
+  // TODO: 갱신시에 products에서 product 재고가 0면 자기를 삭제해주는 자동로직을 넣어준다.
+  return {
+    rootElement: {
+      children: Object.values(products).map((product) => (
+        `<tr>
+          <td>${product.name}</td>
+          <td>${product.price}</td>
+          <td>${product.quantity}</td>
+          <td><button>구매하기</button></td>
+        </tr>`
+      )).join(''),
+    }
+  }
 }
 
+const restAmountState = {};
+
+// 자판기가 보유한 동전에서 최소 갯수의 동전으로 잔돈을 돌려준다.
+// 잔돈으로 반환할 수 있는 금액만 반환한다. (나머지는 무시한다.)
+// 동전은 xx개로 표현한다.
+
+// 반환한 금액만큼 사용자가 충전한 금액이 차감됨.
+// 반환한 금액만큼 자판기의 동전도 차감됨.
+// 반환된 동전의 결과는 누적되지 않는다. (항상 새롭게 받아 계산한다.)
 export function restAmountFlushButtonComponent() {
   return {
-
+    rootElement: {
+      events: {click: () =>{
+        // TODO: 가지고 있던 돈을 자판기 동전 method에 넣어서 반환식을 발동시킨다. 반환받은 동전 갯수들을 state에 적용한다.
+        // TODO: 사용자가 넣은 금액들은 모두 초기화 시킨다.
+        render(REST_AMOUNT_FLUSH_DISPLAY_BINDER);
+      }},
+    }
   }
 }
 
 export function restAmountFlushDisplayComponent() {
+  // TODO: 남은 금액들을 받아서 동전별로 구분해서 보여준다.
   return {
 
   }
