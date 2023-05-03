9376daccc5d251e489257eeea363d443b6a42841
smchoxx <grit.developer.asher@gmail.com>
2023-05-03 19:23:41 +0900

    선택자 객체를 정의하고 객체를 동결화한다

---
+++ src/js/constants/selector.js
@@ -0,0 +1,54 @@
+export default Object.freeze({
+  // 공통
+  vendingMachineApp: 'app',
+  tabContentContainerId: 'tab-content-container',
+
+  // 탭 메뉴 버튼
+  tabButtonContainerId: 'tab-button-container',
+  productManageMenuId: 'product-manage-menu', // 상품관리
+  vendingMachineManageMenuId: 'vending-machine-manage-menu',// 잔동충전
+  productPurchaseMenuId: 'product-purchase-menu', // 상품구매
+
+  // 상품 관리(추가) 메뉴
+  productNameInputId: 'product-name-input',
+  productPriceInputId: 'product-price-input',
+  productQuantityInputId: 'product-quantity-input',
+  productAddButtonId: 'product-add-button',
+  productContainer: 'product-container',
+  productInventory: 'product-inventory',
+  productInventoryContainer: 'product-inventory-container',
+  productManageItemClass: 'product-manage-item',
+  productManageNameClass: 'product-manage-name',
+  productManagePriceClass: 'product-manage-price',
+  productManageQuantityClass: 'product-manage-quantity',
+
+  // 잔돈 충전(자판기 보유 동전) 메뉴
+  vendingMachineChargeInputId: 'vending-machine-charge-input',
+  vendingMachineChargeButtonId: 'vending-machine-charge-button',
+  vendingMachineChargeAmountId: 'vending-machine-charge-amount',
+  vendingMachineCoin500QuantityId: 'vending-machine-coin-500-quantity',
+  vendingMachineCoin100QuantityId: 'vending-machine-coin-100-quantity',
+  vendingMachineCoin50QuantityId: 'vending-machine-coin-50-quantity',
+  vendingMachineCoin10QuantityId: 'vending-machine-coin-10-quantity',
+  vendingMachineCoinTableBody: 'vending-machine-coin-table-body',
+
+  // 상품 구매 메뉴
+  chargeInputId: 'charge-input',
+  chargeButtonId: 'charge-button',
+  chargeAmountId: 'charge-amount',
+  coinReturnButtonId: 'coin-return-button',
+  coin500QuantityId: 'coin-500-quantity',
+  coin100QuantityId: 'coin-100-quantity',
+  coin50QuantityId: 'coin-50-quantity',
+  coin10QuantityId: 'coin-10-quantity',
+  productPurchaseItemClass: 'product-purchase-item',
+  purchaseButtonClass: 'purchase-button',
+  productPurchaseNameClass: 'product-purchase-name',
+  productPurchasePriceClass: 'product-purchase-price',
+  productPurchaseQuantityClass: 'product-purchase-quantity',
+  productNameDataset: 'data-product-name',
+  productPriceDataset: 'data-product-price',
+  productQuantityDataset: 'data-product-quantity',
+  purchaseProductTableBodyId: 'purchase-product-table-body',
+  returnCoinTableBodyId: 'return-coin-table-body',
+});
