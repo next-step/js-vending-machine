b65e6fba36055723098d3a2842614cf804a18cb1
smchoxx <grit.developer.asher@gmail.com>
2023-05-06 19:37:09 +0900

    초기화면으로 상품관리 탭을 표시한다

--- src/js/controllers/VendingMachineController.js
+++ src/js/controllers/VendingMachineController.js
@@ -1,8 +1,10 @@
 import VendingMachineView from '../views/VendingMachineView.js';
+import ProductManageController from './ProductManageController.js';
 
 class VendingMachineController {
   constructor() {
     this.vendingMachineView = new VendingMachineView();
+    this.productManageController = new ProductManageController();
   }
 }
 
