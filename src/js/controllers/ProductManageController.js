bb33322aa9c3950869a7d8cfaa4eb2fc0642daa6
smchoxx <grit.developer.asher@gmail.com>
2023-06-04 13:16:58 +0900

    validate을 통과한 product가 멤버로 저장되도록한다
    - validate 로직을 controller로 이동시킨다

--- src/js/controllers/ProductManageController.js
+++ src/js/controllers/ProductManageController.js
@@ -1,7 +1,7 @@
 import ProductManageView from '../views/ProductManageView.js';
 import ProductManageModel from '../models/ProductManageModel.js';
 import SELECTOR from '../constants/selector.js';
-import {$} from '../utils/dom.js';
+import { $ } from '../utils/dom.js';
 import { getStorageProducts, setStorageProducts } from '../utils/storage.js';
 
 class ProductManageController {
@@ -18,16 +18,13 @@ class ProductManageController {
   }
 
   #initAddEventListener() {
-    $(`#${SELECTOR.tabContentContainerId}`).addEventListener(
-      'click',
-      (e) => this.#onClickTabContent(e),
+    $(`#${SELECTOR.tabContentContainerId}`).addEventListener('click', (e) =>
+      this.#onClickTabContent(e)
     );
   }
 
   #renderTableWithProducts() {
-    this.productManageView.renderTableWithProductItems(
-      getStorageProducts()
-    );
+    this.productManageView.renderTableWithProductItems(getStorageProducts());
   }
 
   #onClickTabContent(e) {
@@ -38,9 +35,10 @@ class ProductManageController {
   #handleProductsMangement() {
     const hasProductInStorage = !!getStorageProducts().length;
     const productInputData = this.#getProductInputData();
+    this.#validate(productInputData);
     const productMangageModel = new ProductManageModel(productInputData);
 
-    if(hasProductInStorage) {
+    if (hasProductInStorage) {
       this.#updateProducts(productInputData);
     } else {
       this.#setProducts(productMangageModel.product);
@@ -58,7 +56,7 @@ class ProductManageController {
     return {
       name: productName.value,
       price: productPrice.value,
-      quantity: productQuantity.value
+      quantity: productQuantity.value,
     };
   }
 
@@ -68,15 +66,15 @@ class ProductManageController {
   }
 
   #isDuplicateProduct(name) {
-    return this.#products.findIndex(it => it.name === name) !== -1;
+    return this.#products.findIndex((it) => it.name === name) !== -1;
   }
 
   #updateProducts(product) {
     const isDuplicateProduct = this.#isDuplicateProduct(product.name);
 
-    if(isDuplicateProduct) {
-      const newProducts = this.#products.map(it => {
-        if(it.name === product.name){
+    if (isDuplicateProduct) {
+      const newProducts = this.#products.map((it) => {
+        if (it.name === product.name) {
           it = product;
         }
 
@@ -87,6 +85,45 @@ class ProductManageController {
       setStorageProducts(newProducts);
     } else {
       this.#setProducts(product);
+    }
+  }
+
+  #validate(productInputData) {
+    this.#validateEmpty(productInputData);
+    this.#validateQuantity(productInputData);
+    this.#validatePrice(productInputData);
+  }
+
+  #validateEmpty(productInputData) {
+    if (!productInputData.name) {
+      alert('상품명을 입력해주세요');
+      throw Error('상품명을 입력해주세요');
+    }
+    if (!productInputData.price) {
+      alert('금액을 입력해주세요');
+      throw Error('금액을 입력해주세요');
+    }
+    if (!productInputData.quantity) {
+      alert('수량을 입력해주세요');
+      throw Error('수량을 입력해주세요');
+    }
+  }
+
+  #validatePrice(productInputData) {
+    if (productInputData.price < 100) {
+      alert('상품의 최소 가격은 100원입니다.');
+      throw Error('상품의 최소 가격은 100원입니다.');
+    }
+    if (productInputData.price % 10 !== 0) {
+      alert('상품의 가격은 10의 배수만 단위만 가능합니다.');
+      throw Error('상품의 가격은 10의 배수만 단위만 가능합니다.');
+    }
+  }
+
+  #validateQuantity(productInputData) {
+    if (productInputData.quantity < 1) {
+      alert('상품의 최소 수량은 1개 이상입니다.');
+      throw Error('상품의 최소 수량은 1개 이상입니다.');
     }
   }
 }
--- src/js/models/ProductManageModel.js
+++ src/js/models/ProductManageModel.js
@@ -3,50 +3,10 @@ class ProductManageModel {
 
   constructor(product) {
     this.#product = product;
-    this.#validate();
   }
 
   get product() {
     return this.#product;
-  }
-
-  #validate() {
-    this.#validateEmpty();
-    this.#validateQuantity();
-    this.#validatePrice();
-  }
-
-  #validateEmpty() {
-    if(!this.#product?.name){
-      alert('상품명을 입력해주세요');
-      throw Error('상품명을 입력해주세요');
-    }
-    if(!this.#product?.price){
-      alert('금액을 입력해주세요');
-      throw Error('금액을 입력해주세요');
-    }
-    if(!this.#product?.quantity){
-      alert('수량을 입력해주세요');
-      throw Error('수량을 입력해주세요');
-    }
-  }
-
-  #validatePrice() {
-    if(this.#product?.price < 100) {
-      alert('상품의 최소 가격은 100원입니다.');
-      throw Error('상품의 최소 가격은 100원입니다.');
-    }
-    if(this.#product?.price % 10 !== 0) {
-      alert('상품의 가격은 10의 배수만 단위만 가능합니다.');
-      throw Error('상품의 가격은 10의 배수만 단위만 가능합니다.');
-    }
-  }
-
-  #validateQuantity() {
-    if(this.#product?.quantity < 1) {
-      alert('상품의 최소 수량은 1개 이상입니다.');
-      throw Error('상품의 최소 수량은 1개 이상입니다.');
-    }
   }
 }
 export default ProductManageModel;
